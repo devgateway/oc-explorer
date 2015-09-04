ef92e828ce5255a9ffa5aca01867a44af34d87d5
Alexei <savca.alexei@gmail.com>
2015-09-04 15:12:16 +0300

    Added a subcomponent example

--- ui/components/app/index.jsx
+++ ui/components/app/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-require('./example.less');
+import Counter from "./counter";
+require('./style.less');
 
 export default class App extends React.Component{
   render(){
@@ -7,10 +8,10 @@ export default class App extends React.Component{
       <div className="container">
         <div className="col-md-12">
           <div className="jumbotron">
-            <h1>I'm a placeholder</h1>
+            <h1>I'm just on example</h1>
             <p>Edit or delete me in ./components/app</p>
             <p>Documentation is in readme.md</p>
-            <p><a className="btn btn-primary btn-lg" href="#" role="button">Awesome!</a></p>
+            <Counter/>
           </div>
         </div>
       </div>
