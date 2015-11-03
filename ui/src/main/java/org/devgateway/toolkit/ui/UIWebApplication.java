287f81a20139d0cf6fb3dad382fcbed0fe72349a
mpostelnicu <mpostelnicu@gmail.com>
2015-11-03 19:28:04 +0200

    default files for sprin boot and WAR servlet 3.0 behavior

---
+++ ui/src/main/java/org/devgateway/toolkit/ui/UIWebApplication.java
@@ -0,0 +1,18 @@
+package org.devgateway.toolkit.ui;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @author mpostelnicu
+ *
+ */
+
+@SpringBootApplication
+@PropertySource("classpath:/org/devgateway/toolkit/ui/application.properties")
+public class UIWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UIWebApplication.class, args);
+	}
+}
