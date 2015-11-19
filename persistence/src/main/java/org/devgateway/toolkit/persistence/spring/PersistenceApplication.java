c4d330ebb20ba60722d79e077404f67b0d0f0583
mpostelnicu <mpostelnicu@developmentgateway.org>
2015-11-19 18:05:02 +0200

    support for mongodb persistence

--- persistence/src/main/java/org/devgateway/toolkit/persistence/spring/PersistenceApplication.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/spring/PersistenceApplication.java
@@ -17,6 +17,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -29,6 +30,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackageClasses = RoleRepository.class)
 @EnableTransactionManagement
 @EntityScan(basePackageClasses = GenericPersistable.class)
+@PropertySource("classpath:/org/devgateway/toolkit/persistence/application.properties")
 @ComponentScan("org.devgateway.toolkit")
 public class PersistenceApplication {
 	
