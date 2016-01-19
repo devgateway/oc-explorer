020f418747a9258e6e9b6b85c26dcde7a0852172
Alexei <savca.alexei@gmail.com>
2016-01-20 00:29:18 +0200

    Implemented year filtering for <FundingByBidType/>

--- ui/components/tender/index.jsx
+++ ui/components/tender/index.jsx
@@ -3,15 +3,16 @@ import Component from "../pure-render-component";
 import CostEffectiveness from "./cost-effectiveness";
 import BiddingPeriod from "./bidding-period";
 import FundingByBidType from "./funding-by-bid-type";
+import {toImmutable} from "nuclear-js";
 
 export default class Tender extends Component{
   render(){
     var {state} = this.props;
     var globalState = state.get('globalState');
     var selectedYears = globalState.get('selectedYears');
-    var width = state.getIn(['globalState', 'contentWidth']);
-    var data = state.getIn(['globalState', 'data']);
-    var year = state.getIn(['globalState', 'year']);
+    var width = globalState.get('contentWidth');
+    var data = globalState.get('data');
+    var year = globalState.get('year');
     return (
         <div className="col-sm-12 content">
           <CostEffectiveness
@@ -21,7 +22,25 @@ export default class Tender extends Component{
                   ceData
               , [])}/>
           <BiddingPeriod width={width}/>
-          <FundingByBidType width={width} data={data.getIn(['bidType', year], [])}/>
+          <FundingByBidType
+              width={width}
+              data={data.get('bidType')
+                  .reduce((data, yearData, year) => selectedYears.get(year) ?
+                      data.concat(yearData) :
+                      data
+                  , toImmutable([]))
+                  .groupBy(bidType => bidType.get('_id'))
+                  .map(bidTypes => bidTypes.reduce((reducedBidType, bidType) => {
+                    return {
+                      _id: bidType.get('_id') || "unspecified",
+                      totalTenderAmount: reducedBidType.totalTenderAmount + bidType.get('totalTenderAmount')
+                    }
+                  }, {
+                    totalTenderAmount: 0
+                  }))
+                  .toArray()
+              }
+          />
         </div>
     )
   }
