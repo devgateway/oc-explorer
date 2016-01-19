aab44d67bfd07615ec67407c3f620387c58ba1ba
Alexei <savca.alexei@gmail.com>
2016-01-19 23:04:51 +0200

    OCVN-58 Cost effectiveness data is now filtered based on selected years before being passed downstream to <CostEffectiveness/>

--- ui/components/tender/index.jsx
+++ ui/components/tender/index.jsx
@@ -7,12 +7,19 @@ import FundingByBidType from "./funding-by-bid-type";
 export default class Tender extends Component{
   render(){
     var {state} = this.props;
+    var globalState = state.get('globalState');
+    var selectedYears = globalState.get('selectedYears');
     var width = state.getIn(['globalState', 'contentWidth']);
     var data = state.getIn(['globalState', 'data']);
     var year = state.getIn(['globalState', 'year']);
     return (
         <div className="col-sm-12 content">
-          <CostEffectiveness width={width} data={data.get('costEffectiveness')}/>
+          <CostEffectiveness
+              width={width}
+              data={selectedYears.reduce((ceData, selected, year) => selected ?
+                  ceData.concat([data.getIn(['costEffectiveness', year])]) :
+                  ceData
+              , [])}/>
           <BiddingPeriod width={width}/>
           <FundingByBidType width={width} data={data.getIn(['bidType', year], [])}/>
         </div>
