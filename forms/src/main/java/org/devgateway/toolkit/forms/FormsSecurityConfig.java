ad95df1b4c2fae47eff922bb15ae59a5b99ffc81
mpostelnicu <mpostelnicu@gmail.com>
2016-01-22 19:56:49 +0200

    OCDS-19 added open access for /api/ocds/release/all

--- forms/src/main/java/org/devgateway/toolkit/forms/FormsSecurityConfig.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/FormsSecurityConfig.java
@@ -51,6 +51,7 @@ public class FormsSecurityConfig extends WebSecurityConfig {
 				"/api/tenderBidPeriodPercentiles/**","/api/ocds/release/budgetProjectId/**",
 				"/api/ocds/release/planningBidNo/**", "/api/plannedFundingByLocation/**",
 				"/api/costEffectivenessAwardAmount/**","/api/costEffectivenessTenderAmount/**",
+				"/api/ocds/release/all/**",
 				
 				"/img/**", "/css*/**", "/js*/**", "/assets*/**", "/wicket/resource/**/*.js",
 				"/wicket/resource/**/*.css", "/wicket/resource/**/*.png", "/wicket/resource/**/*.jpg",
