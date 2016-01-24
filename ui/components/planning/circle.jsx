3883d0b081683f11e24b3a4f7c1a28b932f1b7e8
Alexei <savca.alexei@gmail.com>
2016-01-24 22:07:45 +0200

    OCVN-54 Overriding React d3's <Circle/> Implemented <CircleGroup/> and <CircleCollection/> based on the overrided <Circle/>

---
+++ ui/components/planning/circle.jsx
@@ -0,0 +1,66 @@
+"use strict";
+
+import {
+  default as React,
+  Component,
+  PropTypes
+} from 'react';
+
+import d3 from 'd3';
+
+import {
+  default as ReactFauxDOM,
+} from 'react-faux-dom';
+
+class Circle extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  _mkCircle(dom) {
+    const {
+      data,
+      circleClass,
+      geoPath,
+      r,
+      x,
+      y,
+      onMouseOut,
+      onMouseOver
+    } = this.props;
+
+    var circle = d3.select(dom);
+
+    circle
+      .datum(data)
+      .attr('class', `${circleClass} bubble`)
+      .attr("transform", (d) => { return `translate(${x}, ${y})`})
+      .attr("r", r)
+      .on("mouseover", function (d, i) {return onMouseOver(this, d, i);})
+      .on("mouseout", function (d, i) {return onMouseOut(this, d, i);})
+
+    return circle;
+  }
+
+  render() {
+    var circle = ReactFauxDOM.createElement('circle');
+    var chart = this._mkCircle(circle)
+
+    return chart.node().toReact();
+  }
+}
+
+Circle.defaultProps = {
+  centroidClass: 'react-d3-map-core__centroid',
+  dy: '.35em',
+  onMouseOver: (d) => {},
+  onMouseOut: (d) => {}
+};
+
+Circle.propTypes = {
+  data: PropTypes.object.isRequired,
+  geoPath: PropTypes.func.isRequired,
+  circleClass: PropTypes.string
+};
+
+export default Circle;
