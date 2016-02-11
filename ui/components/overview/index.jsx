db04ce903f79ea1d7dbcdc169ee30826731ca114
Alexei <savca.alexei@gmail.com>
2016-02-11 15:43:17 +0200

    OCVN-69 Implemented overview chart

--- ui/components/overview/index.jsx
+++ ui/components/overview/index.jsx
@@ -1,10 +1,14 @@
 import Component from "../pure-render-component";
+import OverviewChart from "./overview-chart";
 
 export default class Overview extends Component{
   render(){
+    var globalState = this.props.state.get('globalState');
+    var data = globalState.get('data');
+    var width = globalState.get('contentWidth');
     return (
         <div className="col-sm-12 content">
-          here gon be overview
+          <OverviewChart width={width} data={data.get('overview', null)}/>
         </div>
     )
   }
