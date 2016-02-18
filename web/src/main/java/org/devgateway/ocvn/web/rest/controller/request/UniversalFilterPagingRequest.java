5cca928e5ec4574465a255f25c0d544d567d8904
mpostelnicu <mpostelnicu@gmail.com>
2016-02-18 16:33:51 +0100

    OCVN-94 added more filters to the universal request obj

--- web/src/main/java/org/devgateway/ocvn/web/rest/controller/request/UniversalFilterPagingRequest.java
+++ web/src/main/java/org/devgateway/ocvn/web/rest/controller/request/UniversalFilterPagingRequest.java
@@ -5,6 +5,7 @@ package org.devgateway.ocvn.web.rest.controller.request;
 
 import java.util.List;
 
+import cz.jirutka.validator.collection.constraints.EachPattern;
 import cz.jirutka.validator.collection.constraints.EachRange;
 
 /**
@@ -13,12 +14,18 @@ import cz.jirutka.validator.collection.constraints.EachRange;
  */
 public class UniversalFilterPagingRequest extends GenericPagingRequest {
 
-
-	@EachRange(min=1900,max=2200)
+	@EachRange(min = 1900, max = 2200)
 	List<Integer> year;
+
+	@EachPattern(regexp = "^[a-zA-Z0-9]*$")
+	List<String> bidTypeId;
+
+	@EachPattern(regexp = "^[a-zA-Z0-9]*$")
+	List<String> procuringEntityId;
 	
-	
-	
+	@EachPattern(regexp = "^[a-zA-Z0-9]*$")
+	List<String> bidSelectionMethod;
+
 	public UniversalFilterPagingRequest() {
 		super();
 	}
