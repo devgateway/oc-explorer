bcd593b6f376fa1be4f987d1f0a58359d2c4a99a
mpostelnicu <mpostelnicu@gmail.com>
2016-02-18 21:13:08 +0100

    OCVN-94 remove bloat and speed writing of custom queries

---
+++ persistence-mongodb/src/main/java/org/devgateway/toolkit/persistence/mongo/aggregate/CustomGroupingOperation.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.devgateway.toolkit.persistence.mongo.aggregate;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+/**
+ * @author mihai
+ *
+ */
+public class CustomGroupingOperation extends CustomOperation {
+
+	/**
+	 * @param operation
+	 */
+	public CustomGroupingOperation(DBObject operation) {
+		super(new BasicDBObject("$group", operation));
+	}
+
+}
