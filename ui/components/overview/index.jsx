4df4a26a38530121abd74ff1e2412f4b60c0acd8
Alexei <savca.alexei@gmail.com>
2016-03-11 16:24:29 +0200

    OCVN-87 Placing awards and tenders table in one row

--- ui/components/overview/index.jsx
+++ ui/components/overview/index.jsx
@@ -19,8 +19,12 @@ export default class Overview extends Component{
     return (
         <div className="col-sm-12 content">
           <OverviewChart width={width} data={filteredOverviewData}/>
-          <AwardsTable data={data.get('topAwards')}/>
-          <TendersTable data={data.get('topTenders')}/>
+          <div className="col-sm-6">
+            <TendersTable data={data.get('topTenders')}/>
+          </div>
+          <div className="col-sm-6">
+            <AwardsTable data={data.get('topAwards')}/>
+          </div>
         </div>
     )
   }
