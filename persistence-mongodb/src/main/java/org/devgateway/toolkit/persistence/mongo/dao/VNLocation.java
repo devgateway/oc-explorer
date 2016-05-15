3758052f5203739f0780f21a90bc5beb56579ca3
mpostelnicu <mpostelnicu@gmail.com>
2016-05-15 20:39:59 +0300

    OCVN-115 removed unrequired geojson sphere index

--- persistence-mongodb/src/main/java/org/devgateway/toolkit/persistence/mongo/dao/VNLocation.java
+++ persistence-mongodb/src/main/java/org/devgateway/toolkit/persistence/mongo/dao/VNLocation.java
@@ -4,8 +4,6 @@
 package org.devgateway.toolkit.persistence.mongo.dao;
 
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
-import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
-import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
@@ -18,7 +16,7 @@ public class VNLocation extends Location<GeoJsonPoint> {
 	public static final String GEONAMES_URI_PREFIX = "http://www.geonames.org/";
 	public static final String GEONAMES_SCHEME = "GEONAMES";
 
-	@GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
+
 	private GeoJsonPoint geometry;
 
 	public VNLocation() {
