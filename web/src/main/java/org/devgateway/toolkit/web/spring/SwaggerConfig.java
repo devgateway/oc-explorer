265c554699ad8e2f9a01070e9f5afe6e50acd8ba
mpostelnicu <mpostelnicu@gmail.com>
2016-05-17 22:24:47 +0300

    OCVN-163 added api info and swagger ui only for /api/ links

--- web/src/main/java/org/devgateway/toolkit/web/spring/SwaggerConfig.java
+++ web/src/main/java/org/devgateway/toolkit/web/spring/SwaggerConfig.java
@@ -3,18 +3,30 @@ package org.devgateway.toolkit.web.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).groupName("ocDashboardsApi").apiInfo(ocDashboardsApiInfo())
+				.select().apis(RequestHandlerSelectors.any()).paths(regex("/api/.*"))
+				// .apis(RequestHandlerSelectors.any())
+				// .paths(PathSelectors.any())
+				.build();
 	}
+
+	private ApiInfo ocDashboardsApiInfo() {
+		return new ApiInfoBuilder().title("Open Contracting Dashboards API")
+				.description("These endpoints are used to feed the open contracting dashboard").license("MIT")
+				.licenseUrl("https://opensource.org/licenses/MIT").version("1.0").build();
+	}
+
 }
