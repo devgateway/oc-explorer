17cd5ba435682b3a11c5f42f2e775a0154121839
mpostelnicu <mpostelnicu@gmail.com>
2016-05-24 17:07:45 +0300

    OCE-15 generic tests for org and release saving

---
+++ persistence-mongodb/src/test/java/org/devgateway/ocds/persistence/mongo/test/ReleaseRepositoryTest.java
@@ -0,0 +1,33 @@
+package org.devgateway.ocds.persistence.mongo.test;
+
+import org.devgateway.ocds.persistence.mongo.Release;
+import org.devgateway.ocds.persistence.mongo.repository.ReleaseRepository;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.junit.Assert;
+
+public class ReleaseRepositoryTest extends AbstractMongoTest {
+
+	private String OCID="release-1";
+	
+	@Autowired
+	private ReleaseRepository releaseRepository;
+
+	@Test
+	public void testReleaseSaveAndFind() {
+
+			Release release=new Release();
+			
+			release.setOcid(OCID);
+			
+			releaseRepository.insert(release);
+			
+			
+			Release byOcid = releaseRepository.findByOcid(OCID);
+			
+			Assert.assertEquals(byOcid.getOcid(),OCID);
+
+	}
+
+}
