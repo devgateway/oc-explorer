632b02b0712a751ecacde19ce6136154f4a0fce7
Ionut Dobre <idobre@developmentgateway.org>
2016-05-25 12:02:35 -0400

    set properties files for tests.

--- persistence/src/test/java/org/devgateway/toolkit/persistence/test/AbstractPersistenceTest.java
+++ persistence/src/test/java/org/devgateway/toolkit/persistence/test/AbstractPersistenceTest.java
@@ -2,14 +2,11 @@ package org.devgateway.toolkit.persistence.test;
 
 import org.devgateway.toolkit.persistence.spring.PersistenceApplication;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-/**
- * 
- */
 
 /**
  * @author mpostelnicu
@@ -17,8 +14,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @Configuration
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { PersistenceApplication.class })
+@SpringApplicationConfiguration(classes = { PersistenceApplication.class })
 @ActiveProfiles("integration")
+@TestPropertySource("classpath:test.properties")
 public abstract class AbstractPersistenceTest {
 
 }
