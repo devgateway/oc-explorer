9f245a3b6d5ba4ac766b2d45101e19b54c993cc6
Ionut Dobre <idobre@developmentgateway.org>
2016-06-01 16:07:56 -0400

    added @WebIntegrationTest annotation to web tests.

--- web/src/test/java/org/devgateway/toolkit/web/AbstractWebTest.java
+++ web/src/test/java/org/devgateway/toolkit/web/AbstractWebTest.java
@@ -3,6 +3,7 @@ package org.devgateway.toolkit.web;
 import org.devgateway.toolkit.web.spring.WebApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -13,6 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(classes = { WebApplication.class })
 @ActiveProfiles("integration")
 @TestPropertySource("classpath:test.properties")
+@WebIntegrationTest
 public abstract class AbstractWebTest {
 
 }
