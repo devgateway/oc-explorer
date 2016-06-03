e26dea1e51fc860b7f74b0e5fc6487e70964d58c
Alexei <savca.alexei@gmail.com>
2016-06-03 16:50:17 +0300

    Added translation example

--- ui/components/app/index.jsx
+++ ui/components/app/index.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import Counter from "./counter";
 import Component from "../pure-render-component";
+import translatable from "../translatable";
 require('./style.less');
 
-export default class App extends React.Component{
+export default class App extends translatable(React.Component){
   render(){
     var {state, actions} = this.props;
     return (
       <div className="container">
         <div className="col-md-12">
           <div className="jumbotron">
-            <h1>I'm just an example</h1>
-            <p>Edit or delete me in ./components/app</p>
-            <p>Documentation is in readme.md</p>
+            <h1>{this.__("I'm just an example")}</h1>
+            <p>{this.__("Edit or delete me in ./components/app")}</p>
+            <p>{this.__("Documentation is in readme.md")}</p>
             <Counter value={state.getIn(['globalState', 'counter'])} actions={actions}/>
           </div>
+        </div>
+        <div className="col-md-12">
+          {this.__("Language:")}&nbsp;
+          <a href="javascript:void(0);" onClick={e => actions.setLocale("en")}>English</a>
+          &nbsp;|&nbsp;
+          <a href="javascript:void(0);" onClick={e => actions.setLocale("ro")}>Română</a>
         </div>
       </div>
     )
--- ui/flux/actions/constants.es6
+++ ui/flux/actions/constants.es6
@@ -2,5 +2,6 @@ import keyMirror from 'react/lib/keyMirror'
 
 export default keyMirror({
   INC_COUNTER: null,
-  DEC_COUNTER: null
+  DEC_COUNTER: null,
+  LOCALE_CHANGED: null
 })
