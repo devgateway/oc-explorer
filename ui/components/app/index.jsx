75308f116905e1dde409f2170844b290d6af37d3
Alexei <savca.alexei@gmail.com>
2016-06-07 03:21:30 +0300

    OCVN-13 <App/> is now translation ready

--- ui/components/app/index.jsx
+++ ui/components/app/index.jsx
@@ -5,7 +5,6 @@ import Overview from "../overview";
 import Planning from "../planning";
 import Tender from "../tender";
 import NavigationLink from "./navigation-link";
-import {years} from "../../tools";
 import cn from "classnames";
 import {toImmutable} from "nuclear-js";
 import Filters from "./filters";
@@ -30,22 +29,24 @@ export default class App extends translatable(Component){
           <div className="row">
             <section className="col-sm-12 branding">
               <h1>
-                E-procurement
-                <small>Toolkit</small>
+                {this.__('E-procurement')}
+                <small>{this.__('Toolkit')}</small>
               </h1>
             </section>
             <div role="navigation">
-              {navigationLink("Overview", 'search', tabs.OVERVIEW)}
-              {navigationLink("Planning", 'map-marker', tabs.PLANNING)}
-              {navigationLink("Tender", 'time', tabs.TENDER_AWARD)}
+              {navigationLink(this.__("Overview"), 'search', tabs.OVERVIEW)}
+              {navigationLink(this.__("Planning"), 'map-marker', tabs.PLANNING)}
+              {navigationLink(this.__("Tender"), 'time', tabs.TENDER_AWARD)}
             </div>
             <section className="col-sm-12 description">
-              <p><strong>Toolkit description</strong></p>
+              <p><strong>{this.__("Toolkit description")}</strong></p>
               <p>
                 <small>
+                  {this.__(`
                   The Procurement M&E Prototype is an interactive platform for analyzing, monitoring, and evaluating
                   information on procurement in Vietnam. All data in the dashboard are collected from the Vietnam
                   Government eProcurement system (eGP).
+                  `)}
                 </small>
               </p>
             </section>
