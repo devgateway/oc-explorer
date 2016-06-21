1e2e874089da978ba573ab0af107a8d2e836819f
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-06-21 12:22:28 +0300

    OCE-33 quality indicator for average tender period

--- web/src/main/java/org/devgateway/ocds/web/rest/controller/AverageTenderAndAwardPeriods.java
+++ web/src/main/java/org/devgateway/ocds/web/rest/controller/AverageTenderAndAwardPeriods.java
@@ -11,13 +11,23 @@
  *******************************************************************************/
 package org.devgateway.ocds.web.rest.controller;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.limit;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.skip;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.sort;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.unwind;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
-import io.swagger.annotations.ApiOperation;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.Valid;
 
 import org.devgateway.ocds.web.rest.controller.request.DefaultFilterPagingRequest;
 import org.devgateway.toolkit.persistence.mongo.aggregate.CustomOperation;
+import org.devgateway.toolkit.persistence.mongo.aggregate.CustomProjectionOperation;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
@@ -27,18 +37,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.Arrays;
-import java.util.List;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.limit;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.skip;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.sort;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.unwind;
-import static org.springframework.data.mongodb.core.query.Criteria.where;
+import io.swagger.annotations.ApiOperation;
 
 /**
  *
@@ -72,9 +74,45 @@ public class AverageTenderAndAwardPeriods extends GenericOCDSController {
 		Aggregation agg = newAggregation(
 				match(where("tender.tenderPeriod.startDate").exists(true).and("tender.tenderPeriod.endDate")
 						.exists(true).andOperator(getDefaultFilterCriteria(filter))),
-				new CustomOperation(new BasicDBObject("$project", project)),
+				new CustomProjectionOperation(project),
 				group("$year").avg("$tenderLengthDays").as("averageTenderDays"),
 				sort(Direction.DESC, "averageTenderDays"), skip(filter.getSkip()), limit(filter.getPageSize()));
+
+		AggregationResults<DBObject> results = mongoTemplate.aggregate(agg, "release", DBObject.class);
+		List<DBObject> list = results.getMappedResults();
+		return list;
+	}
+	
+	
+
+	@ApiOperation(value = "Quality indicator for averageTenderPeriod endpoint, "
+			+ "showing the percentage of tenders that have start and end dates vs the total tenders in the system")
+	@RequestMapping(value = "/api/qualityAverageTenderPeriod", 
+	method = RequestMethod.GET, produces = "application/json")
+	public List<DBObject> qualityAverageTenderPeriod(@ModelAttribute @Valid final DefaultFilterPagingRequest filter) {
+
+		DBObject project = new BasicDBObject();
+		project.put(Fields.UNDERSCORE_ID, 0);
+		project.put("tenderWithStartEndDates",
+				new BasicDBObject("$cond",
+						Arrays.asList(
+								new BasicDBObject("$and", Arrays.asList(
+										new BasicDBObject("$gt", Arrays.asList("$tender.tenderPeriod.startDate", null)),
+										new BasicDBObject("$gt", Arrays.asList("$tender.tenderPeriod.endDate", null)))),
+								1, 0)));
+
+		DBObject project1 = new BasicDBObject();
+		project1.put("totalTenderWithStartEndDates", 1);
+		project1.put("totalTenders", 1);
+		project1.put("percentageTenderWithStartEndDates",
+				new BasicDBObject("$multiply", Arrays.asList(
+						new BasicDBObject("$divide", Arrays.asList("$totalTenderWithStartEndDates", "$totalTenders")),
+						100)));
+
+		Aggregation agg = newAggregation(match(getDefaultFilterCriteria(filter)),
+				new CustomProjectionOperation(project),
+				group().sum("tenderWithStartEndDates").as("totalTenderWithStartEndDates").count().as("totalTenders"),
+				new CustomProjectionOperation(project1));
 
 		AggregationResults<DBObject> results = mongoTemplate.aggregate(agg, "release", DBObject.class);
 		List<DBObject> list = results.getMappedResults();
