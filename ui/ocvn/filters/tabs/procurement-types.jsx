34860976b4b0827e8c3277280f8034e5e6cf6465
Alexei <savca.alexei@gmail.com>
2016-07-23 01:09:55 +0300

    Refactor procurement types tab

--- ui/ocvn/filters/tabs/procurement-types.jsx
+++ ui/ocvn/filters/tabs/procurement-types.jsx
@@ -1,11 +1,13 @@
 import Tab from "../../../oce/filters/tabs";
 import BidTypes from "../bid-types";
 import {Set} from "immutable";
+import BidSelectionMethod from "../bid-selection-method";
 
 class ProcurementTypes extends Tab{
   render(){
     let {state, onUpdate, bidTypes} = this.props;
     let selectedBidTypesIds = state.get('bidTypeId', Set());
+    let selectedBidSelectionMethods = state.get("bidSelectionMethod", Set());
     return <div>
       <BidTypes
           options={bidTypes}
@@ -13,6 +15,14 @@ class ProcurementTypes extends Tab{
           onToggle={id => onUpdate('bidTypeId', selectedBidTypesIds.has(id) ?
               selectedBidTypesIds.delete(id) :
               selectedBidTypesIds.add(id))
+          }
+      />
+
+      <BidSelectionMethod
+          selected={selectedBidSelectionMethods}
+          onToggle={id => onUpdate('bidSelectionMethod', selectedBidSelectionMethods.has(id) ?
+              selectedBidSelectionMethods.delete(id) :
+              selectedBidSelectionMethods.add(id))
           }
       />
     </div>
