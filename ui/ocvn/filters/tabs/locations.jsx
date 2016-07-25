c00d1851ac0e4ba3b3fdfdc82c3588bd6e85b679
Alexei <savca.alexei@gmail.com>
2016-07-25 15:00:50 +0300

    OCVN-203 Locations filter

--- ui/ocvn/filters/tabs/locations.jsx
+++ ui/ocvn/filters/tabs/locations.jsx
@@ -1,9 +1,24 @@
 import Tab from "../../../oce/filters/tabs";
+import Locations from '../locations';
+import {Set} from "immutable";
 
-class Locations extends Tab{
-
+class LocationsTab extends Tab{
+  render(){
+    let {state, onUpdate, translations} = this.props;
+    let selectedLocations = state.get('tenderDeliveryLocationIdentifier', Set());
+    return <div>
+      <Locations
+          selected={selectedLocations}
+          onToggle={id => onUpdate('tenderDeliveryLocationIdentifier', selectedLocations.has(id) ?
+              selectedLocations.delete(id) :
+              selectedLocations.add(id))
+          }
+          translations={translations}
+      />
+    </div>
+  }
 }
 
-Locations.getName = __ => __('Locations');
+LocationsTab.getName = __ => __('Locations');
 
-export default Locations;
+export default LocationsTab;
