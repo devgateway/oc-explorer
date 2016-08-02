52dedf2ae86ae4325dedb5300bc8c4021a98580b
Alexei <savca.alexei@gmail.com>
2016-08-02 17:01:28 +0300

    OCE-57 Added a default layout for OCE

--- ui/index.jsx
+++ ui/index.jsx
@@ -1,22 +1,94 @@
-import App from "./components/app";
-import React from "react";
 import ReactDOM from "react-dom";
-import flux from "./flux";
-import {debounce} from "./tools";
-var TRANSLATIONS = {
-  en: require('./languages/en_US.json'),
-  vn: require('./languages/vn_VN.json')
-};
+import OCApp from "./oce";
+import OverviewTab from './oce/tabs/overview';
+import LocationTab from './oce/tabs/location';
+import CompetitivenessTab from './oce/tabs/competitiveness';
+import EfficiencyTab from './oce/tabs/efficiency';
+import EProcurementTab from './oce/tabs/e-procurement';
+import {fetchJson} from "./oce/tools";
+import {Map} from "immutable";
+import styles from "./style.less";
 
-flux.onUpdate(state =>
-  ReactDOM.render(
-    <App state={state} actions={flux.actions} translations={TRANSLATIONS[state.getIn(['globalState', 'locale'])]}/>,
-    document.getElementById('dg-container')
-  )
-, true);
+function getBidTypeDescription(__, {id, description}){
+  switch(+id){
+    case 12: return __("Unspecified") + " #1";
+    case 15: return __("Unspecified") + " #2";
+    default: return description;
+  }
+}
 
-window.addEventListener("resize", debounce(function(){
-  flux.actions.changeContentWidth(document.querySelector('.years-bar').offsetWidth)
-}));
+class OCUS extends OCApp{
+  constructor(props) {
+    super(props);
+    this.registerTab(OverviewTab);
+    this.registerTab(LocationTab);
+    this.registerTab(CompetitivenessTab);
+    this.registerTab(EfficiencyTab);
+    this.registerTab(EProcurementTab);
+  }
 
-flux.actions.bootstrap();
+  fetchBidTypes(){
+    fetchJson('/api/ocds/bidType/all').then(data =>
+        this.setState({
+          bidTypes: data.reduce((map, datum) =>
+              map.set(datum.id, getBidTypeDescription(this.__.bind(this), datum)), Map())
+        })
+    );
+  }
+
+  render(){
+    return <div className="container-fluid" onClick={_ => this.setState({menuBox: ""})}>
+      <header className="branding row">
+        <div className="col-sm-offset-1 col-sm-4">
+          <h1>
+            {this.__('e-Procurement')}
+            <small>{this.__('Toolkit')}</small>
+          </h1>
+        </div>
+        <div className="col-sm-6 menu">
+          {this.filters()}
+          {this.comparison()}
+          {this.exportBtn()}
+        </div>
+        <div className="col-sm-2 language-switcher">
+          {this.languageSwitcher()}
+        </div>
+      </header>
+      <aside className="col-xs-4 col-md-3 col-lg-2">
+        <div className="row">
+          <div role="navigation">
+            {this.navigation()}
+          </div>
+          <section className="col-sm-12 description">
+            <h3><strong>{this.__("Toolkit description")}</strong></h3>
+            <p>
+              <small>
+                {this.__("The Procurement M&E Prototype is an interactive platform for analyzing, monitoring, and evaluating information on procurement in Vietnam. All data in the dashboard are collected from the Vietnam Government eProcurement system (eGP).")}
+              </small>
+            </p>
+          </section>
+        </div>
+      </aside>
+      <div className="col-xs-offset-4 col-md-offset-3 col-lg-offset-2 col-xs-8 col-md-9 col-lg-10">
+        <div className="row">
+          {this.content()}
+        </div>
+      </div>
+      <div className="col-xs-offset-4 col-md-offset-3 col-lg-offset-2 col-xs-8 col-md-9 col-lg-10 years-bar" role="navigation">
+        {this.yearsBar()}
+      </div>
+      <footer className="col-sm-12 main-footer">&nbsp;</footer>
+    </div>;
+  }
+}
+
+ReactDOM.render(<OCUS/>, document.getElementById('dg-container'));
+
+if("ocvn.developmentgateway.org" == location.hostname){
+  (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+  ga('create','UA-78202947-1');ga('send','pageview');
+}
