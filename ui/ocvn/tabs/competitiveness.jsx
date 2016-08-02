d413605fe27ae3d5bdcc434fba660783c1a39ca8
Alexei <savca.alexei@gmail.com>
2016-08-02 16:53:23 +0300

    OCE-57 Cleanup

--- ui/ocvn/tabs/competitiveness.jsx
+++ ui/ocvn/tabs/competitiveness.jsx
@@ -3,9 +3,7 @@ import CostEffectiveness from "../../oce/visualizations/charts/cost-effectivenes
 import BidSelectionMethod from "../visualizations/bid-selection-method";
 import AvgNrBids from "../../oce/visualizations/charts/avg-nr-bids";
 
-
 class OCVNCompetitiveness extends Competitiveness{}
 
-OCVNCompetitiveness.icon = "competitive";
 OCVNCompetitiveness.visualizations = [CostEffectiveness, BidSelectionMethod, AvgNrBids];
 export default OCVNCompetitiveness;
