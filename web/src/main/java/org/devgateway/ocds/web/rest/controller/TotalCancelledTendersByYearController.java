f690aa58215ae30df8fbc9cd7ff517fd00ae1bc5
mpostelnicu <mpostelnicu@gmail.com>
2016-08-19 19:07:47 +0300

    OCVN-145

--- web/src/main/java/org/devgateway/ocds/web/rest/controller/TotalCancelledTendersByYearController.java
+++ web/src/main/java/org/devgateway/ocds/web/rest/controller/TotalCancelledTendersByYearController.java
@@ -11,13 +11,19 @@
  *******************************************************************************/
 package org.devgateway.ocds.web.rest.controller;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.sort;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
-import io.swagger.annotations.ApiOperation;
+import java.util.List;
+
+import javax.validation.Valid;
 
 import org.devgateway.ocds.web.rest.controller.request.DefaultFilterPagingRequest;
 import org.devgateway.toolkit.persistence.mongo.aggregate.CustomOperation;
+import org.devgateway.toolkit.persistence.mongo.aggregate.CustomProjectionOperation;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort.Direction;
@@ -29,14 +35,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.List;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.sort;
-import static org.springframework.data.mongodb.core.query.Criteria.where;
+import io.swagger.annotations.ApiOperation;
 
 /**
  *
@@ -65,6 +67,35 @@ public class TotalCancelledTendersByYearController extends GenericOCDSController
 				match(where("tender.status").is("cancelled").and("tender.tenderPeriod.startDate").exists(true)),
 				getMatchDefaultFilterOperation(filter), new CustomOperation(new BasicDBObject("$project", project)),
 				group("$year").sum("$tender.value.amount").as("totalCancelledTendersAmount"),
+				sort(Direction.ASC, Fields.UNDERSCORE_ID));
+
+        AggregationResults<DBObject> results = mongoTemplate.aggregate(agg, "release", DBObject.class);
+        List<DBObject> list = results.getMappedResults();
+        return list;
+    }
+	
+	@ApiOperation(value = "Total Cancelled tenders by year by cancel reason. "
+			+ "The tender amount is read from tender.value."
+			+ "The tender status has to be 'cancelled'. The year is retrieved from tender.tenderPeriod.startDate."
+			+ "The cancellation reason is read from tender.cancellationRationale.")
+	@RequestMapping(value = "/api/totalCancelledTendersByYearByRationale", method = { RequestMethod.POST,
+			RequestMethod.GET }, produces = "application/json")
+	public List<DBObject> totalCancelledTendersByYearByRationale(
+			@ModelAttribute @Valid final DefaultFilterPagingRequest filter) {
+
+        DBObject year = new BasicDBObject("$year", "$tender.tenderPeriod.startDate");
+
+        DBObject project = new BasicDBObject();
+        project.put(Fields.UNDERSCORE_ID, 0);
+        project.put("year", year);
+        project.put("tender.value.amount", 1);
+        project.put("tender.cancellationRationale", 1);
+
+		Aggregation agg = newAggregation(
+				match(where("tender.status").is("cancelled").and("tender.tenderPeriod.startDate").exists(true)),
+				getMatchDefaultFilterOperation(filter), new CustomProjectionOperation(project),
+				group("$year", "$tender.cancellationRationale").sum("$tender.value.amount")
+						.as("totalCancelledTendersAmount"),
 				sort(Direction.ASC, Fields.UNDERSCORE_ID));
 
         AggregationResults<DBObject> results = mongoTemplate.aggregate(agg, "release", DBObject.class);
