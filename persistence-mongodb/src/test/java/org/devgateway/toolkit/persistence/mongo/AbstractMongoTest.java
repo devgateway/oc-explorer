bdb3f9967636f1bc82f217fe9a9b91e8d1fa6d9e
Ionut Dobre <idobre@developmentgateway.org>
2016-09-13 15:40:22 -0400

    refactor AbstractMongoTest class.

--- persistence-mongodb/src/test/java/org/devgateway/toolkit/persistence/mongo/AbstractMongoTest.java
+++ persistence-mongodb/src/test/java/org/devgateway/toolkit/persistence/mongo/AbstractMongoTest.java
@@ -1,8 +1,9 @@
-package org.devgateway.toolkit.persistence.mongo.test;
+package org.devgateway.toolkit.persistence.mongo;
 
 import org.devgateway.toolkit.persistence.mongo.spring.MongoPersistenceApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -13,6 +14,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(classes = { MongoPersistenceApplication.class })
 @ActiveProfiles("integration")
 @TestPropertySource("classpath:test.properties")
+@WebIntegrationTest
+/**
+ * Superclass for all integration tests that use MongoDB.
+ * Adding this as a superclass will ensure the tests are run with the "integration" spring profile.
+ * This will enable a special embedded mongodb server that can be used just during the test. 
+ *
+ * @author mihai
+ *
+ */
 public abstract class AbstractMongoTest {
-
+    protected boolean testDataInitialized;
 }
