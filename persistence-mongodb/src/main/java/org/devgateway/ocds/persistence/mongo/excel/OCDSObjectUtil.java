464b35ccc002ce7cf7ba915d43b691316e17ab17
Ionut Dobre <idobre@developmentgateway.org>
2016-09-14 00:03:57 -0400

    added a DefaultLocation class.

--- persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/excel/OCDSObjectUtil.java
+++ persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/excel/OCDSObjectUtil.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.log4j.Logger;
+import org.devgateway.ocds.persistence.mongo.DefaultLocation;
+import org.devgateway.ocds.persistence.mongo.Location;
 import org.devgateway.ocds.persistence.mongo.Release;
 import org.devgateway.ocds.persistence.mongo.excel.annotation.ExcelExportSepareteSheet;
 import org.devgateway.ocds.persistence.mongo.info.ClassFields;
@@ -168,5 +170,6 @@ public final class OCDSObjectUtil {
      *      .put(Budget.class, VNBudget.class)
      */
     public static final ImmutableMap<Class, Class> INHERITEDOCDSOBJECTS = new ImmutableMap.Builder<Class, Class>()
+            .put(Location.class, DefaultLocation.class)
             .build();
 }
