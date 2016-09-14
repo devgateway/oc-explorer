f1e56dafebabbf0a92a00ceeb458079e3cfa05e1
Ionut Dobre <idobre@developmentgateway.org>
2016-09-14 13:43:43 -0400

    OCE-64 - added testes for TopTenController.

--- web/src/test/java/org/devgateway/ocds/web/rest/controller/TopTenControllerTest.java
+++ web/src/test/java/org/devgateway/ocds/web/rest/controller/TopTenControllerTest.java
@@ -1,7 +1,9 @@
 package org.devgateway.ocds.web.rest.controller;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import org.devgateway.ocds.web.rest.controller.request.YearFilterPagingRequest;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,7 +24,19 @@ public class TopTenControllerTest extends AbstractEndPointControllerTest {
         final List<DBObject> topTenLargestAwards = topTenController
                 .topTenLargestAwards(new YearFilterPagingRequest());
 
-        logger.error(topTenLargestAwards);
+        Assert.assertEquals(2, topTenLargestAwards.size());
+
+        final DBObject first = topTenLargestAwards.get(0);
+        BasicDBObject awards = (BasicDBObject) first.get("awards");
+        BasicDBObject value = (BasicDBObject) awards.get("value");
+        double amount = (double) value.get("amount");
+        Assert.assertEquals(6000000.0, amount, 0);
+
+        final DBObject second = topTenLargestAwards.get(1);
+        awards = (BasicDBObject) second.get("awards");
+        value = (BasicDBObject) awards.get("value");
+        amount = (double) value.get("amount");
+        Assert.assertEquals(40000.0, amount, 0);
     }
 
     @Test
@@ -30,6 +44,24 @@ public class TopTenControllerTest extends AbstractEndPointControllerTest {
         final List<DBObject> topTenLargestTenders = topTenController
                 .topTenLargestTenders(new YearFilterPagingRequest());
 
-        logger.error(topTenLargestTenders);
+        Assert.assertEquals(3, topTenLargestTenders.size());
+
+        final DBObject first = topTenLargestTenders.get(0);
+        BasicDBObject tender = (BasicDBObject) first.get("tender");
+        BasicDBObject value = (BasicDBObject) tender.get("value");
+        double amount = (double) value.get("amount");
+        Assert.assertEquals(600000.0, amount, 0);
+
+        final DBObject second = topTenLargestTenders.get(1);
+        tender = (BasicDBObject) second.get("tender");
+        value = (BasicDBObject) tender.get("value");
+        amount = (double) value.get("amount");
+        Assert.assertEquals(400000.0, amount, 0);
+
+        final DBObject third = topTenLargestTenders.get(2);
+        tender = (BasicDBObject) third.get("tender");
+        value = (BasicDBObject) tender.get("value");
+        amount = (double) value.get("amount");
+        Assert.assertEquals(9000.0, amount, 0);
     }
 }
