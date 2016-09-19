e3d9dcdc44b060b846a057c98dff44fb86973b6c
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-09-19 16:26:07 +0300

    #103 upgraded EntityScan package

--- persistence/src/main/java/org/devgateway/toolkit/persistence/spring/PersistenceApplication.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/spring/PersistenceApplication.java
@@ -15,7 +15,7 @@ import org.devgateway.toolkit.persistence.dao.GenericPersistable;
 import org.devgateway.toolkit.persistence.repository.RoleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -33,7 +33,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EntityScan(basePackageClasses = GenericPersistable.class)
 @PropertySource("classpath:/org/devgateway/toolkit/persistence/application.properties")
-@ComponentScan("org.devgateway.toolkit")
+@ComponentScan("org.devgatewray.toolkit")
 public class PersistenceApplication {
 	
 	public static void main(final String[] args) {
