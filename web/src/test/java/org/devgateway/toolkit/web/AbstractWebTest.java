7e7e21ec665e42e174c3ea3b5e647fa5c989a622
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-09-20 19:18:24 +0300

    fixed tests #105

--- web/src/test/java/org/devgateway/toolkit/web/AbstractWebTest.java
+++ web/src/test/java/org/devgateway/toolkit/web/AbstractWebTest.java
@@ -1,28 +1,19 @@
 package org.devgateway.toolkit.web;
 
-import liquibase.servicelocator.LiquibaseService;
 import org.apache.log4j.Logger;
 import org.devgateway.toolkit.web.spring.WebApplication;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@TestConfiguration
 @RunWith(SpringRunner.class)
-// @ActiveProfiles("integration")
-// @LiquibaseService(skip=true)
-@SpringBootTest(classes = {WebApplication.class},
-        webEnvironment=WebEnvironment.RANDOM_PORT,
-        properties={"classpath:test.properties"})
-public class AbstractWebTest {
-    protected static Logger logger = Logger.getLogger(AbstractWebTest.class);
+@ActiveProfiles("integration")
+@SpringBootTest(classes = { WebApplication.class }, webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:test.properties")
+public abstract class AbstractWebTest {
+	protected static Logger logger = Logger.getLogger(AbstractWebTest.class);
 
-    @Test
-    public void test() {
-        logger.error(">>>>");
-    }
 }
