b403209e5dee62fee4c4889e8c67720e25dca0b4
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-09-21 12:37:00 +0300

    #105

--- persistence/src/test/java/org/devgateway/toolkit/persistence/test/AbstractPersistenceTest.java
+++ persistence/src/test/java/org/devgateway/toolkit/persistence/test/AbstractPersistenceTest.java
@@ -4,6 +4,7 @@ import org.devgateway.toolkit.persistence.spring.PersistenceApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -12,7 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @ActiveProfiles("integration")
-@SpringBootTest(classes = {PersistenceApplication.class}, properties={"classpath:test.properties"})
+@SpringBootTest(classes = { PersistenceApplication.class })
+@TestPropertySource("classpath:test.properties")
 public abstract class AbstractPersistenceTest {
 
 }
