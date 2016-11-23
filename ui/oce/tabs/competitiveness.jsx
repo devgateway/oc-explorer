f882f4e0c8fad17ab70fb96dc3ff32cb3eb2dfe6
Alexei <savca.alexei@gmail.com>
2016-11-23 15:45:06 +0200

    OCE-148 Added `frequent tenderers` table

--- ui/oce/tabs/competitiveness.jsx
+++ ui/oce/tabs/competitiveness.jsx
@@ -2,11 +2,12 @@ import Tab from "./index";
 import CostEffectiveness from "../visualizations/charts/cost-effectiveness";
 import AvgNrBids from "../visualizations/charts/avg-nr-bids";
 import ProcurementMethod from "../visualizations/charts/procurement-method";
+import FrequentTenderers from "../visualizations/tables/frequent-tenderers";
 
 class Competitiveness extends Tab{
   static getName(t){return t('tabs:competitiveness:title')}
 }
 
 Competitiveness.icon = "competitive";
-Competitiveness.visualizations = [CostEffectiveness, ProcurementMethod, AvgNrBids];
+Competitiveness.visualizations = [CostEffectiveness, ProcurementMethod, AvgNrBids, FrequentTenderers];
 export default Competitiveness;
