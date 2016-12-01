aa08e6de173cff2b14e2f4bc9bf0bf29e26d878e
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-12-01 17:46:42 +0200

    OCE-151 OCE-145 added predicates

--- persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/flags/preconditions/FlaggedReleasePredicates.java
+++ persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/flags/preconditions/FlaggedReleasePredicates.java
@@ -1,5 +1,6 @@
 package org.devgateway.ocds.persistence.mongo.flags.preconditions;
 
+import org.devgateway.ocds.persistence.mongo.Award;
 import org.devgateway.ocds.persistence.mongo.FlaggedRelease;
 import org.devgateway.ocds.persistence.mongo.Tender.ProcurementMethod;
 
@@ -14,16 +15,24 @@ public final class FlaggedReleasePredicates {
 
     }
 
-    public static final NamedPredicate<FlaggedRelease> HAS_TENDER_START_DATE = new NamedPredicate<>(
-            "HAS_TENDER_START_DATE", p -> p.getTender() != null && p.getTender().getTenderPeriod() != null
+    public static final NamedPredicate<FlaggedRelease> TENDER_START_DATE = new NamedPredicate<>(
+            "Needs to have tender start date", p -> p.getTender() != null && p.getTender().getTenderPeriod() != null
                     && p.getTender().getTenderPeriod().getStartDate() != null);
 
-    public static final NamedPredicate<FlaggedRelease> HAS_TENDER_END_DATE =
-            new NamedPredicate<>("HAS_TENDER_END_DATE", p -> p.getTender() != null
+    public static final NamedPredicate<FlaggedRelease> TENDER_END_DATE =
+            new NamedPredicate<>("Needs to have tender end date", p -> p.getTender() != null
                     && p.getTender().getTenderPeriod() != null && p.getTender().getTenderPeriod().getEndDate() != null);
 
-    public static final NamedPredicate<FlaggedRelease> HAS_OPEN_PROCUREMENT_METHOD =
-            new NamedPredicate<>("HAS_OPEN_PROCUREMENT_METHOD",
+    public static final NamedPredicate<FlaggedRelease> OPEN_PROCUREMENT_METHOD =
+            new NamedPredicate<>("Needs to have open tender procurement method",
                     p -> p.getTender() != null && ProcurementMethod.open.equals(p.getTender().getProcurementMethod()));
+
+    public static final NamedPredicate<FlaggedRelease> ACTIVE_AWARD =
+            new NamedPredicate<>("Needs to have at least one active award;",
+                    p -> p.getAwards().stream().filter(a -> Award.Status.active.equals(a.getStatus())).count() > 0);
+
+    public static final NamedPredicate<FlaggedRelease> UNSUCCESSFUL_AWARD = new NamedPredicate<>(
+            "Needs to have at least one unsuccessful award",
+            p -> p.getAwards().stream().filter(a -> Award.Status.unsuccessful.equals(a.getStatus())).count() > 0);
 
 }
