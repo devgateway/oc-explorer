7de233acbdb61df817414599f98d5e4b652317c3
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-12-22 13:13:43 +0200

    OCE-161 i004 flag

--- persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/flags/FlagsConstants.java
+++ persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/flags/FlagsConstants.java
@@ -8,4 +8,5 @@ public final class FlagsConstants {
 
     public static final String I038_VALUE = "flags.i038.value";
     public static final String I007_VALUE = "flags.i007.value";
+    public static final String I004_VALUE = "flags.i004.value";
 }
