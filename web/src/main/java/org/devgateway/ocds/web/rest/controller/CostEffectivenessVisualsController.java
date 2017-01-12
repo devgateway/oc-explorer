dffac31ee6a1d5f2580b522fd674e39e7a845e0f
Mihai Postelnicu <mpostelnicu@gmail.com>
2017-01-12 14:53:57 +0200

    OCE-175

--- web/src/main/java/org/devgateway/ocds/web/rest/controller/CostEffectivenessVisualsController.java
+++ web/src/main/java/org/devgateway/ocds/web/rest/controller/CostEffectivenessVisualsController.java
@@ -80,8 +80,8 @@ public class CostEffectivenessVisualsController extends GenericOCDSController {
         public static final String TOTAL_TENDER_WITH_AWARDS = "totalTenderWithAwards";
         public static final String PERCENTAGE_TENDERS_WITH_AWARDS = "percentageTendersWithAwards";
         private static final String FRACTION_TENDERS_WITH_AWARDS = "fractionTendersWithAwards";
-        public static final String PERCENTAGE_AWARD_TENDER_AMOUNT = "percentageAwardTenderAmount";
-        public static final String PERCENTAGE_DIFF_TENDER_AMOUNT = "percentageDiffTenderAmount";
+        public static final String PERCENTAGE_AWARD_AMOUNT = "percentageAwardAmount";
+        public static final String PERCENTAGE_DIFF_AMOUNT = "percentageDiffAmount";
         public static final String DIFF_TENDER_AWARD_AMOUNT = "diffTenderAwardAmount";
         private static final String YEAR_MONTH = "year-month"; //this is for internal use
         public static final String MONTH = "month";
@@ -258,22 +258,31 @@ public class CostEffectivenessVisualsController extends GenericOCDSController {
         Collection<DBObject> respCollection = response.values();
 
         respCollection.forEach(dbobj -> {
-            dbobj.put(Keys.DIFF_TENDER_AWARD_AMOUNT,
-                    BigDecimal
-                            .valueOf(dbobj.get(Keys.TOTAL_TENDER_AMOUNT) == null ? 0d
-                                    : ((Number) dbobj.get(Keys.TOTAL_TENDER_AMOUNT)).doubleValue())
-                            .subtract(BigDecimal.valueOf(dbobj.get(Keys.TOTAL_AWARD_AMOUNT) == null ? 0d
-                                    : ((Number) dbobj.get(Keys.TOTAL_AWARD_AMOUNT)).doubleValue())));
 
-            dbobj.put(Keys.PERCENTAGE_AWARD_TENDER_AMOUNT,
-                    dbobj.get(Keys.TOTAL_TENDER_AMOUNT)!=null
-                            && !dbobj.get(Keys.TOTAL_TENDER_AMOUNT).equals(BigDecimal.ZERO)?
-                    BigDecimal
-                            .valueOf(dbobj.get(Keys.TOTAL_AWARD_AMOUNT) == null ? 0d
-                                    : ((Number) dbobj.get(Keys.TOTAL_AWARD_AMOUNT)).doubleValue())
-                            .divide(BigDecimal.valueOf(dbobj.get(Keys.TOTAL_TENDER_AMOUNT) == null ? 0d
-                                    : ((Number) dbobj.get(Keys.TOTAL_TENDER_AMOUNT)).doubleValue()),BigDecimal.ROUND_HALF_UP)
-                            .multiply(ONE_HUNDRED):BigDecimal.ZERO);
+            BigDecimal totalTenderAmount = BigDecimal
+                    .valueOf(dbobj.get(Keys.TOTAL_TENDER_AMOUNT) == null ? 0d
+                            : ((Number) dbobj.get(Keys.TOTAL_TENDER_AMOUNT)).doubleValue());
+
+            BigDecimal totalAwardAmount = BigDecimal
+                    .valueOf(dbobj.get(Keys.TOTAL_AWARD_AMOUNT) == null ? 0d
+                            : ((Number) dbobj.get(Keys.TOTAL_AWARD_AMOUNT)).doubleValue());
+
+            dbobj.put(Keys.DIFF_TENDER_AWARD_AMOUNT,
+                    totalTenderAmount
+                            .subtract(totalAwardAmount));
+
+            dbobj.put(Keys.PERCENTAGE_AWARD_AMOUNT,
+                    totalTenderAmount.compareTo(BigDecimal.ZERO) != 0
+                            ? (totalAwardAmount.setScale(15)
+                                    .divide(totalTenderAmount, BigDecimal.ROUND_HALF_UP)
+                                    .multiply(ONE_HUNDRED)) : BigDecimal.ZERO);
+
+            dbobj.put(Keys.PERCENTAGE_DIFF_AMOUNT,
+                    totalTenderAmount.compareTo(BigDecimal.ZERO) != 0
+                            ? (((BigDecimal) dbobj.get(Keys.DIFF_TENDER_AWARD_AMOUNT)).setScale(15)
+                            .divide(totalTenderAmount, BigDecimal.ROUND_HALF_UP)
+                            .multiply(ONE_HUNDRED)) : BigDecimal.ZERO);
+
         });
 
         return new ArrayList<>(respCollection);
