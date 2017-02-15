2f12bade295ad6461ae97457b5012a1a576a92a2
Mihai Postelnicu <mpostelnicu@gmail.com>
2017-02-15 17:43:23 +0200

    OCVN-285 evict /languages/ folder from spring security

--- web/src/main/java/org/devgateway/toolkit/web/spring/WebSecurityConfig.java
+++ web/src/main/java/org/devgateway/toolkit/web/spring/WebSecurityConfig.java
@@ -73,7 +73,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(final WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/", "/home", "/v2/api-docs/**", "/swagger-ui.html**", "/webjars/**", "/images/**",
-                "/configuration/**", "/swagger-resources/**", "/dashboard",
+                "/configuration/**", "/swagger-resources/**", "/dashboard", "/languages/**",
                 "/wicket/resource/**/*.ttf", "/wicket/resource/**/*.woff",
                 "/wicket/resource/**/*.woff2", "/wicket/resource/**/*.css.map"
                 ).antMatchers(allowedApiEndpoints);
