c079113b14199b43feeff7daa84b541c5370b925
Alexei Savca <savca.alexei@gmail.com>
2017-03-21 16:15:27 +0200

    OCE-185 Overview table

--- ui/oce/corruption-risk/overview-page.jsx
+++ ui/oce/corruption-risk/overview-page.jsx
@@ -1,6 +1,7 @@
 import Chart from "../visualizations/charts/index";
 import {Map} from "immutable";
 import {pluck} from "../tools";
+import Table from "../visualizations/tables/index";
 
 class CorruptionType extends Chart{
   getData(){
@@ -37,25 +38,85 @@ class CorruptionType extends Chart{
 
 CorruptionType.endpoint = 'totalFlaggedIndicatorsByIndicatorTypeByYear';
 
+class TopFlaggedContracts extends Table{
+  row(entry, index){
+    const tenderValue = entry.getIn(['tender', 'value']);
+    const awardValue = entry.getIn(['awards', 0, 'value']);
+    const tenderPeriod = entry.get('tenderPeriod');
+    const startDate = new Date(tenderPeriod.get('startDate'));
+    const endDate = new Date(tenderPeriod.get('endDate'));
+    return (
+        <tr key={index}>
+          <th></th>
+          <th>{entry.get('ocid')}</th>
+          <th>{entry.get('title')}</th>
+          <th>{entry.getIn(['procuringEntity', 'name'])}</th>
+          <th>{tenderValue.get('amount')} {tenderValue.get('currency')}</th>
+          <th>{awardValue.get('amount')} {awardValue.get('currency')}</th>
+          <th>{startDate.toLocaleDateString()}&mdash;{endDate.toLocaleDateString()}</th>
+          <th>{entry.getIn(['flaggedStats', 'type'])}</th>
+        </tr>
+    )
+  }
+
+  render(){
+    const {data} = this.props;
+    return (
+        <table className="table table-striped table-hover table-top-flagged-contracts">
+          <thead>
+          <tr>
+            <th>Status</th>
+            <th>Contract ID</th>
+            <th>Title</th>
+            <th>Procuring Entity</th>
+            <th>Tender Amount</th>
+            <th>Awards Amount</th>
+            <th>Tender Date</th>
+            <th className="flag-type">Flag Type</th>
+          </tr>
+          </thead>
+          <tbody>
+          {data && data.map(this.row.bind(this))}
+          </tbody>
+        </table>
+    )
+  }
+}
+
+TopFlaggedContracts.endpoint = 'corruptionRiskOverviewTable?pageSize=10';
+
 class OverviewPage extends React.Component{
   constructor(...args){
     super(...args);
     this.state = {
-      corruptionType: null
+      corruptionType: null,
+      topFlaggedContracts: null
     }
   }
 
   render(){
+    const {corruptionType, topFlaggedContracts} = this.state;
     return (
+      <div>
         <section>
           <h4>Corruption Types</h4>
           <CorruptionType
               filters={Map()}
               requestNewData={(_, corruptionType) => this.setState({corruptionType})}
               translations={{}}
-              data={this.state.corruptionType}
+              data={corruptionType}
           />
         </section>
+        <section>
+          <TopFlaggedContracts
+              filters={Map()}
+              data={topFlaggedContracts}
+              translations={{}}
+              years={Map()}
+              requestNewData={(_, topFlaggedContracts) => this.setState({topFlaggedContracts})}
+          />
+        </section>
+      </div>
     )
   }
 }
