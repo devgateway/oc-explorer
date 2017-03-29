04b8c570467599d989af24a6f34b25e8f1b2422e
Mihai Postelnicu <mpostelnicu@gmail.com>
2017-03-29 17:18:34 +0300

    OCE-218 OCE-204

--- web/src/main/java/org/devgateway/ocds/web/rest/controller/flags/AbstractFlagCrosstabController.java
+++ web/src/main/java/org/devgateway/ocds/web/rest/controller/flags/AbstractFlagCrosstabController.java
@@ -24,9 +24,13 @@ import static org.springframework.data.mongodb.core.aggregation.Aggregation.newA
 public abstract class AbstractFlagCrosstabController extends AbstractFlagController {
 
     public static final class GenericKeys {
-        public static final String TOTAL = "total";
+        public static final String PERCENT = "percent";
 
 
+    }
+
+    public String getFlagDesignation(String flagProperty) {
+        return flagProperty.substring(6, flagProperty.length() - 6);
     }
 
 
@@ -39,53 +43,34 @@ public abstract class AbstractFlagCrosstabController extends AbstractFlagControl
 
     protected DBObject getGroup(final YearFilterPagingRequest filter) {
         DBObject group = new BasicDBObject();
-        addYearlyMonthlyReferenceToGroup(filter, group);
-        group.put(GenericKeys.TOTAL, new BasicDBObject("$sum", 1));
-        group.put(FlagsConstants.I004_VALUE, groupSumBoolean(""));
-        group.put(GenericKeys.TOTAL_FALSE, new BasicDBObject("$sum", new BasicDBObject("$cond",
-                Arrays.asList(new BasicDBObject("$eq", Arrays.asList(ref(getFlagProperty()), false)), 1, 0))));
-        group.put(GenericKeys.TOTAL_PRECOND_MET, new BasicDBObject("$sum", new BasicDBObject("$cond",
-                Arrays.asList(new BasicDBObject("$gt", Arrays.asList(ref(getFlagProperty()), null)), 1, 0))));
+        group.put(Fields.UNDERSCORE_ID, null);
+        FlagsConstants.FLAGS_LIST.forEach(f -> group.put(getFlagDesignation(f), groupSumBoolean(f, true)));
         return group;
     }
 
     protected DBObject getProjectPercentage(final YearFilterPagingRequest filter) {
         DBObject project2 = new BasicDBObject();
-        if (filter.getMonthly()) {
-            project2.put("year", 1);
-            project2.put("month", 1);
-        } else {
-            project2.put(Fields.UNDERSCORE_ID, 1);
-        }
-        project2.put(GenericKeys.TOTAL, 1);
-        project2.put(GenericKeys.TOTAL_TRUE, 1);
-        project2.put(GenericKeys.TOTAL_FALSE, 1);
-        project2.put(GenericKeys.TOTAL_PRECOND_MET, 1);
-        project2.put(GenericKeys.PERCENT_TRUE_PRECOND_MET,
-                new BasicDBObject("$cond",
-                        Arrays.asList(new BasicDBObject("$eq", Arrays.asList(ref(GenericKeys.TOTAL_PRECOND_MET),
-                                0)), new BasicDBObject("$literal", 0),
-                                new BasicDBObject("$multiply",
-                                        Arrays.asList(new BasicDBObject("$divide",
-                                                Arrays.asList(ref(GenericKeys.TOTAL_TRUE),
-                                                ref(GenericKeys.TOTAL_PRECOND_MET))), 100))
-                        ))
 
-        );
-        project2.put(GenericKeys.PERCENT_FALSE_PRECOND_MET,
-                new BasicDBObject("$cond",
-                        Arrays.asList(new BasicDBObject("$eq", Arrays.asList(ref(GenericKeys.TOTAL_PRECOND_MET),
-                                0)), new BasicDBObject("$literal", 0),
-                                new BasicDBObject("$multiply",
-                                        Arrays.asList(new BasicDBObject("$divide",
-                                                Arrays.asList(ref(GenericKeys.TOTAL_FALSE),
-                                                ref(GenericKeys.TOTAL_PRECOND_MET))), 100))
-                        ))
+        project2.put(Fields.UNDERSCORE_ID, 0);
+        FlagsConstants.FLAGS_LIST.forEach(f -> {
+            project2.put(getFlagDesignation(f), 1);
+            project2.put(GenericKeys.PERCENT + "." + getFlagDesignation(f),
+                    new BasicDBObject("$cond",
+                            Arrays.asList(new BasicDBObject("$eq",
+                                            Arrays.asList(ref(getFlagDesignation(getFlagProperty())),
+                                                    0)), new BasicDBObject("$literal", 0),
+                                    new BasicDBObject("$multiply",
+                                            Arrays.asList(new BasicDBObject("$divide",
+                                                    Arrays.asList(ref(getFlagDesignation(f)),
+                                                            ref(getFlagDesignation(getFlagProperty())))), 100))
+                            ))
 
-        );
-        project2.put(GenericKeys.PERCENT_PRECOND_MET, new BasicDBObject("$multiply",
-                Arrays.asList(new BasicDBObject("$divide", Arrays.asList(ref(GenericKeys.TOTAL_PRECOND_MET),
-                        ref(GenericKeys.TOTAL))), 100)));
+            );
+
+
+        });
+
+
         return project2;
     }
 
@@ -99,13 +84,12 @@ public abstract class AbstractFlagCrosstabController extends AbstractFlagControl
         DBObject projectPercentage = getProjectPercentage(filter);
 
         Aggregation agg = newAggregation(
-                match(getYearDefaultFilterCriteria(filter, getYearProperty()).and(getYearProperty()).exists(true)),
+                match(getYearDefaultFilterCriteria(filter, getYearProperty())
+                        .and(getYearProperty()).exists(true)
+                        .and(getFlagProperty()).is(true)),
                 new CustomProjectionOperation(projectPrepare),
                 new CustomGroupingOperation(group),
-                new CustomProjectionOperation(projectPercentage),
-                transformYearlyGrouping(filter).andInclude(GenericKeys.TOTAL, GenericKeys.TOTAL_TRUE,
-                        GenericKeys.TOTAL_FALSE, GenericKeys.TOTAL_PRECOND_MET, GenericKeys.PERCENT_TRUE_PRECOND_MET,
-                        GenericKeys.PERCENT_FALSE_PRECOND_MET, GenericKeys.PERCENT_PRECOND_MET)
+                new CustomProjectionOperation(projectPercentage)
         );
 
         AggregationResults<DBObject> results = mongoTemplate.aggregate(agg, "release", DBObject.class);
