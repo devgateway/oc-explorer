51d782293e3aa6ae5d3d13147093fe912e66d18c
Alexei Savca <savca.alexei@gmail.com>
2017-05-02 21:12:16 +0300

    OCE-261 Overview page no longer depends on INDICATOR_NAMES

--- ui/oce/corruption-risk/overview-page.jsx
+++ ui/oce/corruption-risk/overview-page.jsx
@@ -3,7 +3,6 @@ import {pluck, range} from "../tools";
 import Table from "../visualizations/tables/index";
 import ReactDOMServer from "react-dom/server";
 import CustomPopupChart from "./custom-popup-chart";
-import INDICATOR_NAMES from "./indicator-names";
 
 const pluckObj = (field, obj) => Object.keys(obj).map(key => obj[key][field]);
 
@@ -192,7 +191,7 @@ class TopFlaggedContracts extends Table{
                 <hr/>
               </div>
               <div className="col-sm-12 info">
-                {flagIds.map(flagId => <p key={flagId}>{INDICATOR_NAMES[flagId].name}</p>)}
+                {flagIds.map(flagId => <p key={flagId}>{this.t(`crd:indicators:${flagId}:name`)}</p>)}
               </div>
             </div>
             <div className="arrow"/>
