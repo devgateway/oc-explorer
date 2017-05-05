e4feb035c09e5cd83fc31f59c72c202fb115d1f2
Alexei Savca <savca.alexei@gmail.com>
2017-05-05 13:05:56 +0300

    OCE-282 Logo needed a bit of transparency

--- ui/oce/corruption-risk/landing-popup.jsx
+++ ui/oce/corruption-risk/landing-popup.jsx
@@ -2,11 +2,19 @@ class LandingPopup extends React.Component{
   render(){
     return (
       <div>
-        <div className="crd-landing-popup-overlay">
-          
-        </div>
+        <div className="crd-landing-popup-overlay"/>
         <div className="crd-landing-popup">
-          Why hello there!
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm-1">
+                <img src="assets/logo.png"/>
+              </div>
+              <div className="col-sm-11">
+                <h2>Corruption Risk Dashboard</h2>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     )
