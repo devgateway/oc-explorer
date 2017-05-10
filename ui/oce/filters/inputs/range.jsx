114451330e1ce192c646bb4a54961afc86ad659c
Alexei Savca <savca.alexei@gmail.com>
2017-05-10 15:41:06 +0300

    OCE-294 Reindented range.jsx

--- ui/oce/filters/inputs/range.jsx
+++ ui/oce/filters/inputs/range.jsx
@@ -9,24 +9,24 @@ const MILLION = 1000000;
 const THOUSAND = 1000;
 
 let labelFormatter =  number => {
-    if(typeof number == "undefined") return number;
-    if(number >= BILLION) return (number/BILLION).toFixed(2) + "B";
-    if(number >= MILLION) return (number/MILLION).toFixed(2) + "M";
-    if(number >= THOUSAND) return (number/THOUSAND).toFixed(2) + "K";
-    return number.toFixed(2);
+  if(typeof number == "undefined") return number;
+  if(number >= BILLION) return (number/BILLION).toFixed(2) + "B";
+  if(number >= MILLION) return (number/MILLION).toFixed(2) + "M";
+  if(number >= THOUSAND) return (number/THOUSAND).toFixed(2) + "K";
+  return number.toFixed(2);
 }
 
 class Range extends translatable(Component){
   maybeGetInputRange(min, max, minValue, maxValue, onUpdate){
     if(!isIE){
       return (
-          <InputRange
-              minValue={min}
-              maxValue={max}
-              value={{min: minValue, max: maxValue}}
-              onChange={(_, newVal) => onUpdate(newVal, {min, max})}
-              formatLabel={labelFormatter}
-          />
+        <InputRange
+          minValue={min}
+          maxValue={max}
+          value={{min: minValue, max: maxValue}}
+          onChange={(_, newVal) => onUpdate(newVal, {min, max})}
+          formatLabel={labelFormatter}
+        />
       )
     }
     return null;
@@ -39,33 +39,33 @@ class Range extends translatable(Component){
     const minValue = this.props.minValue || min;
     const maxValue = this.props.maxValue || max;
     return (
-        <section className="field">
-          <header>
-            {this.getTitle()}
-          </header>
-          <section className="options range">
-            {this.maybeGetInputRange(min, max, minValue, maxValue, onUpdate)}
-          </section>
-          <div className="range-inputs">
-            {this.t('general:range:min')}
-            &nbsp;
-            <input
-                type="number"
-                className="form-control input-sm"
-                value={minValue}
-                onChange={e => onUpdate({min: +e.target.value, max: maxValue}, {min, max})}
-            />
-            &nbsp;
-            {this.t('general:range:max')}
-            &nbsp;
-            <input
-                type="number"
-                className="form-control input-sm"
-                value={maxValue}
-                onChange={e => onUpdate({min: minValue, max: +e.target.value}, {min, max})}
-            />
-          </div>
+      <section className="field">
+        <header>
+          {this.getTitle()}
+        </header>
+        <section className="options range">
+          {this.maybeGetInputRange(min, max, minValue, maxValue, onUpdate)}
         </section>
+        <div className="range-inputs">
+          {this.t('general:range:min')}
+          &nbsp;
+          <input
+            type="number"
+            className="form-control input-sm"
+            value={minValue}
+            onChange={e => onUpdate({min: +e.target.value, max: maxValue}, {min, max})}
+          />
+        &nbsp;
+      {this.t('general:range:max')}
+        &nbsp;
+      <input
+        type="number"
+        className="form-control input-sm"
+        value={maxValue}
+        onChange={e => onUpdate({min: minValue, max: +e.target.value}, {min, max})}
+      />
+        </div>
+      </section>
     )
   }
 }
