3e02b884c29092e0d17b98d58bb3a35c68adfdbf
Ionut Dobre <idobre@developmentgateway.org>
2017-05-10 14:12:17 -0400

    use js code compatible with old browsers.

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/components/charts/plotlyChart.js
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/components/charts/plotlyChart.js
@@ -1,13 +1,37 @@
 /**
  * init function that is called from the Wicket Component
  */
-const init = function (parameters) {
+var init = function (parameters) {
     'use strict';
 
-    const chart = new PlotlyChart(parameters);
+    var chart = new PlotlyChart(parameters);
     chart.render();
 };
 
+/**
+ * Object.assign for ES5
+ */
+if (typeof Object.assign != 'function') {
+    Object.assign = function(target) {
+        'use strict';
+        if (target == null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        target = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source != null) {
+                for (var key in source) {
+                    if (Object.prototype.hasOwnProperty.call(source, key)) {
+                        target[key] = source[key];
+                    }
+                }
+            }
+        }
+        return target;
+    };
+}
 
 /**
  * Use this if you want to configure some default properties for a plot.ly chart.
@@ -31,7 +55,9 @@ function PlotlyChart(parameters) {
  * Function that actually renders the chart using plot.ly
  */
 PlotlyChart.prototype.render = function() {
-    const { chartId, data, layout } = this.props;
+    var chartId = this.props.chartId;
+    var data = this.props.data;
+    var layout = this.props.layout;
 
     // console.log(JSON.stringify(this.props, null, '\t'));
 
