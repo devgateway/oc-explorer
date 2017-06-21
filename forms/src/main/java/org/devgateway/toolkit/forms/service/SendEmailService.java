654d80a455070c74fa8c4e9d8747b92e035a962c
Mihai Postelnicu <mpostelnicu@gmail.com>
2017-06-21 14:44:57 +0300

    OCVN-401 used interface for mail sender

--- forms/src/main/java/org/devgateway/toolkit/forms/service/SendEmailService.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/service/SendEmailService.java
@@ -15,7 +15,7 @@ import org.devgateway.toolkit.persistence.dao.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 /**
@@ -27,7 +27,7 @@ import org.springframework.stereotype.Component;
 public class SendEmailService {
 
     @Autowired
-    private JavaMailSenderImpl javaMailSenderImpl;
+    private JavaMailSender javaMailSenderImpl;
 
     private SimpleMailMessage templateMessage;
 
