2c4bbc5521329f4dfb153d6f21d64b6270a79ef7
Mihai Postelnicu <mpostelnicu@gmail.com>
2017-06-23 13:10:38 +0300

    OCVN-401 added a warning type runtime exception that will not flag the import process as failed - string constructor

--- persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/reader/ImportWarningRuntimeException.java
+++ persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/reader/ImportWarningRuntimeException.java
@@ -4,4 +4,8 @@ package org.devgateway.ocds.persistence.mongo.reader;
  * Created by mpostelnicu on 6/23/17.
  */
 public class ImportWarningRuntimeException extends RuntimeException {
+
+    public ImportWarningRuntimeException(String var1) {
+        super(var1);
+    }
 }
