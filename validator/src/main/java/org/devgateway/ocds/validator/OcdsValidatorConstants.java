4ace4930fec9743ff8106f51689115d362ca3585
Mihai Postelnicu <mpostelnicu@gmail.com>
2017-07-06 17:47:11 +0300

    OCE-355 added release prefix and subfixes and paths to classpath resources

--- validator/src/main/java/org/devgateway/ocds/validator/OcdsValidatorConstants.java
+++ validator/src/main/java/org/devgateway/ocds/validator/OcdsValidatorConstants.java
@@ -5,6 +5,10 @@ package org.devgateway.ocds.validator;
  */
 public final class OcdsValidatorConstants {
 
+    private OcdsValidatorConstants() {
+
+    }
+
     public static final class Versions {
         public static final String OCDS_1_0_0 = "1.0.0";
         public static final String OCDS_1_0_1 = "1.0.1";
@@ -12,6 +16,19 @@ public final class OcdsValidatorConstants {
         public static final String OCDS_1_1_0 = "1.1.0";
     }
 
+    public static final class Schemas {
+        public static final String RELEASE = "release";
+        public static final String RELEASE_PACKAGE = "release-package";
+        public static final String RECORD_PACKAGE = "record-package";
+        public static final String[] ALL = {RELEASE, RELEASE_PACKAGE, RECORD_PACKAGE};
+    }
 
+    public static final class SchemaPrefixes {
+        public static final String RELEASE = "release/release-schema-";
+        public static final String RELEASE_PACKAGE = "release-package/release-package-schema-";
+        public static final String RECORD_PACKAGE = "record-package/record-package-schema-";
+    }
+
+    public static final String SCHEMA_POSTFIX = ".json";
 
 }
