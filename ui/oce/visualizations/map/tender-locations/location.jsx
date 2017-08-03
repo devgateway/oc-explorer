1018af52409e87398aafe0d6c137de19b26e6a3a
Alexei Savca <savca.alexei@gmail.com>
2017-08-03 14:30:14 +0300

    OCE-354 Moved the chart tools to popup header

--- ui/oce/visualizations/map/tender-locations/location.jsx
+++ ui/oce/visualizations/map/tender-locations/location.jsx
@@ -8,89 +8,14 @@ import OverviewChart from '../../../visualizations/charts/overview';
 import CostEffectiveness from '../../../visualizations/charts/cost-effectiveness';
 import { cacheFn, download } from '../../../tools';
 import ProcurementMethodChart from '../../../visualizations/charts/procurement-method';
-
-class LocationWrapper extends translatable(Component) {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentTab: 0,
-    };
-  }
-
-  render() {
-    const { currentTab } = this.state;
-    const { data, translations, filters, years, styling, monthly, months } = this.props;
-    const CurrentTab = this.constructor.TABS[currentTab];
-    const t = translationKey => this.t(translationKey);
-    return (
-      <Marker {...this.props}>
-        <Popup className="tender-locations-popup">
-          <div>
-            <header>
-              {data.name}
-              <img
-                src="assets/icons/export-black.svg"
-                alt="Export"
-                width="16"
-                height="16"
-              />
-              <img src="assets/icons/camera.svg" alt="Screenshot" />
-            </header>
-            <div className="row">
-              <div className="tabs-bar col-xs-4">
-                {this.constructor.TABS.map((Tab, index) => (
-                  <div
-                    key={Tab.getName(t)}
-                    className={cn({ active: index === currentTab })}
-                    onClick={() => this.setState({ currentTab: index })}
-                    role="button"
-                    tabIndex={0}
-                    >
-                    <a href="#">{Tab.getName(t)}</a>
-                  </div>
-                ))}
-              </div>
-              <div className="col-xs-8">
-                <CurrentTab
-                  data={data}
-                  translations={translations}
-                  filters={filters}
-                  years={years}
-                  monthly={monthly}
-                  months={months}
-                  styling={styling}
-                />
-              </div>
-            </div>
-          </div>
-        </Popup>
-      </Marker>
-    );
-  }
-}
-
-class Tab extends translatable(Component) {}
-
-export class OverviewTab extends Tab {
-  static getName(t) { return t('maps:tenderLocations:tabs:overview:title'); }
-
-  render() {
-    const { data } = this.props;
-    const { count, amount } = data;
-    return (<div>
-      <p>
-        <strong>{this.t('maps:tenderLocations:tabs:overview:nrOfTenders')}</strong> {count}
-      </p>
-      <p>
-        <strong>{this.t('maps:tenderLocations:tabs:overview:totalFundingByLocation')}</strong> {amount.toLocaleString()}
-      </p>
-    </div>);
-  }
-}
+// eslint-disable-next-line no-unused-vars
+import styles from './style.less';
 
 const addTenderDeliveryLocationId = cacheFn(
   (filters, id) => filters.set('tenderLoc', id),
 );
+
+class Tab extends translatable(Component) {}
 
 export class ChartTab extends Tab {
   constructor(props) {
@@ -124,21 +49,115 @@ export class ChartTab extends Tab {
     return (
       <div className={cn('map-chart', this.constructor.getChartClass())}>
         <this.constructor.Chart
-        filters={decoratedFilters}
-        styling={styling}
-        years={years}
-        monthly={monthly}
-        months={months}
-        translations={translations}
-        data={this.state.chartData}
-        requestNewData={(_, chartData) => this.setState({ chartData })}
-        width={500}
-        height={350}
-        margin={this.constructor.getMargins()}
-        legend="h"
+          filters={decoratedFilters}
+          styling={styling}
+          years={years}
+          monthly={monthly}
+          months={months}
+          translations={translations}
+          data={this.state.chartData}
+          requestNewData={(_, chartData) => this.setState({ chartData })}
+          width={500}
+          height={350}
+          margin={this.constructor.getMargins()}
+          legend="h"
         />
       </div>
     );
+  }
+}
+
+class LocationWrapper extends translatable(Component) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTab: 0,
+    };
+  }
+
+  render() {
+    const { currentTab } = this.state;
+    const { data, translations, filters, years, styling, monthly, months } = this.props;
+    const CurrentTab = this.constructor.TABS[currentTab];
+    const t = translationKey => this.t(translationKey);
+    return (
+      <Marker {...this.props}>
+        <Popup className="tender-locations-popup">
+          <div>
+            <header>
+
+              {CurrentTab.prototype instanceof ChartTab &&
+                <span className="chart-tools">
+                  <a tabIndex={-1} role="button">
+                    <img
+                      src="assets/icons/export-black.svg"
+                      alt="Export"
+                      width="16"
+                      height="16"
+                    />
+                  </a>
+                  <a
+                    tabIndex={-1}
+                    role="button"
+                    onClick={() => ReactDOM.findDOMNode(this).querySelector('.modebar-btn:first-child').click()}
+                  >
+                    <img
+                      src="assets/icons/camera.svg"
+
+                      alt="Screenshot"
+                    />
+                  </a>
+                </span>
+              }
+              {data.name}
+            </header>
+            <div className="row">
+              <div className="tabs-bar col-xs-4">
+                {this.constructor.TABS.map((tab, index) => (
+                  <div
+                    key={tab.getName(t)}
+                    className={cn({ active: index === currentTab })}
+                    onClick={() => this.setState({ currentTab: index })}
+                    role="button"
+                    tabIndex={0}
+                  >
+                    <a href="#">{tab.getName(t)}</a>
+                  </div>
+                ))}
+              </div>
+              <div className="col-xs-8">
+                <CurrentTab
+                  data={data}
+                  translations={translations}
+                  filters={filters}
+                  years={years}
+                  monthly={monthly}
+                  months={months}
+                  styling={styling}
+                />
+              </div>
+            </div>
+          </div>
+        </Popup>
+      </Marker>
+    );
+  }
+}
+
+export class OverviewTab extends Tab {
+  static getName(t) { return t('maps:tenderLocations:tabs:overview:title'); }
+
+  render() {
+    const { data } = this.props;
+    const { count, amount } = data;
+    return (<div>
+      <p>
+        <strong>{this.t('maps:tenderLocations:tabs:overview:nrOfTenders')}</strong> {count}
+      </p>
+      <p>
+        <strong>{this.t('maps:tenderLocations:tabs:overview:totalFundingByLocation')}</strong> {amount.toLocaleString()}
+      </p>
+    </div>);
   }
 }
 
