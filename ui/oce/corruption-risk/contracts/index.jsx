8c258ed67fba35767a365539b1c62248a014a5f2
Alexei Savca <savca.alexei@gmail.com>
2017-10-06 15:33:32 +0300

    OCE-377 Implemented contract listing

--- ui/oce/corruption-risk/contracts/index.jsx
+++ ui/oce/corruption-risk/contracts/index.jsx
@@ -1,29 +1,56 @@
+import { List } from 'immutable';
 import CRDPage from '../page';
 import Visualization from '../../visualization';
-import { List } from 'immutable';
 
 class CList extends Visualization {
   render() {
     const { data, navigate } = this.props;
     return (
       <tbody>
-        {data.map(contract => {
+        {data.map((contract) => {
            const id = contract.get('ocid');
-console.log(contract.toJS());
+           const startDate = contract.getIn(['tender', 'tenderPeriod', 'startDate']);
            return (
-             <tr>
-               <td>{data.getIn(['tender', 'status'])}</td>
+             <tr key={id}>
+               <td>{contract.getIn(['tender', 'status'], this.t('general:undefined'))}</td>
+               <td>
+                 <a
+                   href="javascript:void(0);"
+                   onClick={() => navigate('contract', this.t('general:undefined'))}
+                 >
+                   {id}
+                 </a>
+               </td>
                <td>
                  <a
                    href="javascript:void(0);"
                    onClick={() => navigate('contract', id)}
-                   key={id}
                  >
                    {contract.getIn(['tender', 'title'], id)}
                  </a>
                </td>
+               <td>
+                 {contract.getIn(['tender', 'procuringEntity', 'name'], this.t('general:undefined'))}
+               </td>
+               <td>
+                 {contract.getIn(['tender', 'value', 'amount'], this.t('general:undefined'))}
+                 &nbsp;
+                 {contract.getIn(['tender', 'value', 'currency'])}
+               </td>
+               <td>
+                 {contract.getIn(['awards', 0, 'value', 'amount'], this.t('general:undefined'))}
+                 &nbsp;
+                 {contract.getIn(['awards', 0, 'value', 'currency'])}
+               </td>
+               <td>
+                 {startDate ?
+                   new Date(startDate).toLocaleDateString() :
+                   this.t('general:undefined')
+                 }
+               </td>
+               <td />
              </tr>
-           )
+           );
         })}
       </tbody>
     );
@@ -33,34 +60,37 @@ console.log(contract.toJS());
 CList.endpoint = 'ocds/release/all';
 
 export default class Contracts extends CRDPage {
-  constructor(...args){
+  constructor(...args) {
     super(...args);
     this.state = {
-      list: List()
-    }
+      list: List(),
+    };
   }
 
   render() {
     const { list } = this.state;
-    const { filters, navigate } = this.props;
+    const { filters, navigate, translations } = this.props;
     return (
-      <div id="contracts-page">
-        <table className="table">
+      <div className="contracts-page">
+        <table className="table table-striped">
           <thead>
-            <th>{this.t('crd:contracts:baseInfo:status')}</th>
-            <th>{this.t('crd:procurementsTable:contractID')}</th>
-            <th>{this.t('crd:general:contract:title')}</th>
-            <th>{this.t('crd:contracts:list:procuringEntity')}</th>
-            <th>{this.t('crd:procurementsTable:tenderAmount')}</th>
-            <th>{this.t('crd:procurementsTable:awardAmount')}</th>
-            <th>{this.t('crd:procurementsTable:tenderDate')}</th>
-            <th>{this.t('crd:procurementsTable:flagType')}</th>
+            <tr>
+              <th>{this.t('crd:contracts:baseInfo:status')}</th>
+              <th>{this.t('crd:procurementsTable:contractID')}</th>
+              <th>{this.t('crd:general:contract:title')}</th>
+              <th>{this.t('crd:contracts:list:procuringEntity')}</th>
+              <th>{this.t('crd:procurementsTable:tenderAmount')}</th>
+              <th>{this.t('crd:contracts:list:awardAmount')}</th>
+              <th>{this.t('crd:procurementsTable:tenderDate')}</th>
+              <th>{this.t('crd:procurementsTable:flagType')}</th>
+            </tr>
           </thead>
           <CList
             data={list}
             filters={filters}
-            requestNewData = {(_, list) => this.setState({list})}
+            requestNewData={(_, newList) => this.setState({ list: newList })}
             navigate={navigate}
+            translations={translations}
           />
         </table>
       </div>
