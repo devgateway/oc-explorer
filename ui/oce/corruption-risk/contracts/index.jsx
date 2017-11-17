ac0be1021617c58c7005d49de480d6a52118a7d3
Alexei Savca <savca.alexei@gmail.com>
2017-11-17 15:46:18 +0200

    OCE-395 Added react-bootstrap-table

--- ui/oce/corruption-risk/contracts/index.jsx
+++ ui/oce/corruption-risk/contracts/index.jsx
@@ -1,6 +1,8 @@
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import { List } from 'immutable';
-import cn from 'classnames';
 import URI from 'urijs';
+// eslint-disable-next-line no-unused-vars
+import rbtStyles from 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import CRDPage from '../page';
 import Visualization from '../../visualization';
 import TopSearch from './top-search';
@@ -9,76 +11,111 @@ const API_ROOT = '/api';
 
 class CList extends Visualization {
   buildUrl(ep) {
-    let { filters, searchQuery } = this.props;
-    let uri = new URI(API_ROOT + '/' + ep).addSearch(filters.toJS());
+    const { filters, searchQuery } = this.props;
+    const uri = new URI(`${API_ROOT}/${ep}`)
+      .addSearch('pageSize', 20)
+      .addSearch(filters.toJS());
     return searchQuery ?
       uri.addSearch('text', searchQuery) :
       uri;
   }
 
-  componentDidUpdate(prevProps){
+  componentDidUpdate(prevProps) {
     const { filters, searchQuery } = this.props;
-    if (filters != prevProps.filters || searchQuery != prevProps.searchQuery) {
+    if (filters !== prevProps.filters || searchQuery !== prevProps.searchQuery) {
       this.fetch();
     }
   }
 
-  render() {
-    const { data, navigate } = this.props;
+  mkLink(content, { id }) {
+    const { navigate } = this.props;
     return (
-      <tbody>
-        {data.map((contract) => {
-           const id = contract.get('ocid');
-           const startDate = contract.getIn(['tender', 'tenderPeriod', 'startDate']);
-           const flagType = contract.getIn(['flags', 'flaggedStats', 0, 'type']);
-           return (
-             <tr key={id}>
-               <td>{contract.getIn(['tender', 'status'], this.t('general:undefined'))}</td>
-               <td>
-                 <a
-                   href="javascript:void(0);"
-                   onClick={() => navigate('contract', id)}
-                 >
-                   {id}
-                 </a>
-               </td>
-               <td>
-                 <a
-                   href="javascript:void(0);"
-                   onClick={() => navigate('contract', id)}
-                 >
-                   {contract.getIn(['tender', 'title'], id)}
-                 </a>
-               </td>
-               <td>
-                 {contract.getIn(['tender', 'procuringEntity', 'name'], this.t('general:undefined'))}
-               </td>
-               <td>
-                 {contract.getIn(['tender', 'value', 'amount'], this.t('general:undefined'))}
-                 &nbsp;
-                 {contract.getIn(['tender', 'value', 'currency'])}
-               </td>
-               <td>
-                 {contract.getIn(['awards', 0, 'value', 'amount'], this.t('general:undefined'))}
-                 &nbsp;
-                 {contract.getIn(['awards', 0, 'value', 'currency'])}
-               </td>
-               <td>
-                 {startDate ?
-                   new Date(startDate).toLocaleDateString() :
-                   this.t('general:undefined')
-                 }
-               </td>
-               <td>
-                 {flagType ?
-                   this.t(`crd:corruptionType:${flagType}:name`) :
-                   this.t('general:undefined')}
-               </td>
-             </tr>
-           );
-        })}
-      </tbody>
+      <a
+        href="javascript:void(0);"
+        onClick={() => navigate('contract', id)}
+      >
+        {content}
+      </a>
     );
+  }
+
+  render() {
+    const { data } = this.props;
+    if (!data || !data.count()) return null;
+    const jsData = data.map((contract) => {
+      const tenderAmount = contract.getIn(['tender', 'value', 'amount'], 'N/A') +
+          ' ' +
+          contract.getIn(['tender', 'value', 'currency'], '');
+
+      const winningAward = contract.get('awards', List()).find(award => award.get('status') === 'active');
+      let awardAmount = 'N/A';
+      if (winningAward) {
+        awardAmount = winningAward.getIn(['value', 'amount'], 'N/A') +
+          ' ' +
+          winningAward.getIn(['value', 'currency'], '')
+      }
+
+      const startDate = contract.getIn(['tender', 'tenderPeriod', 'startDate']);
+
+      const flagTypes = contract.getIn(['flags', 'flaggedStats'], List())
+        .map(flagType => this.t(`crd:corruptionType:${flagType.get('type')}:name`))
+        .join(', ') || 'N/A';
+
+      return {
+        status: contract.getIn(['tender', 'status'], 'N/A'),
+        id: contract.get('ocid'),
+        title: contract.getIn(['tender', 'title'], 'N/A'),
+        PEName: contract.getIn(['tender', 'procuringEntity', 'name'], 'N/A'),
+        tenderAmount,
+        awardAmount,
+        startDate: startDate ? new Date(startDate).toLocaleDateString() : 'N/A',
+        flagTypes,
+      };
+    }).toJS();
+
+    return (
+      <BootstrapTable
+        data={jsData}
+        striped
+        bordered={false}
+        pagination
+        options={{
+          page: 1,
+        }}
+      >
+        <TableHeaderColumn dataField="status">
+          {this.t('crd:contracts:baseInfo:status')}
+        </TableHeaderColumn>
+
+        <TableHeaderColumn isKey dataField="id" dataFormat={this.mkLink.bind(this)}>
+          {this.t('crd:procurementsTable:contractID')}
+        </TableHeaderColumn>
+
+        <TableHeaderColumn dataField="title" dataFormat={this.mkLink.bind(this)}>
+          {this.t('crd:general:contract:title')}
+        </TableHeaderColumn>
+
+        <TableHeaderColumn dataField="PEName">
+          {this.t('crd:contracts:list:procuringEntity')}
+        </TableHeaderColumn>
+
+        <TableHeaderColumn dataField="tenderAmount">
+          {this.t('crd:procurementsTable:tenderAmount')}
+        </TableHeaderColumn>
+
+        <TableHeaderColumn dataField="awardAmount">
+          {this.t('crd:contracts:list:awardAmount')}
+        </TableHeaderColumn>
+
+        <TableHeaderColumn dataField="startDate">
+          {this.t('crd:procurementsTable:tenderDate')}
+        </TableHeaderColumn>
+
+        <TableHeaderColumn dataField="flagTypes">
+          {this.t('crd:procurementsTable:flagType')}
+        </TableHeaderColumn>
+      </BootstrapTable>
+    )
   }
 }
 
@@ -107,28 +144,14 @@ export default class Contracts extends CRDPage {
           {this.t('crd:contracts:top-search:resultsFor').replace('$#$', searchQuery)}
         </h3>}
 
-        <table className={cn('table', 'table-striped', {hide: !list.count()})}>
-          <thead>
-            <tr>
-              <th>{this.t('crd:contracts:baseInfo:status')}</th>
-              <th>{this.t('crd:procurementsTable:contractID')}</th>
-              <th>{this.t('crd:general:contract:title')}</th>
-              <th>{this.t('crd:contracts:list:procuringEntity')}</th>
-              <th>{this.t('crd:procurementsTable:tenderAmount')}</th>
-              <th>{this.t('crd:contracts:list:awardAmount')}</th>
-              <th>{this.t('crd:procurementsTable:tenderDate')}</th>
-              <th>{this.t('crd:procurementsTable:flagType')}</th>
-            </tr>
-          </thead>
-          <CList
-            data={list}
-            filters={filters}
-            requestNewData={(_, newList) => this.setState({ list: newList })}
-            navigate={navigate}
-            translations={translations}
-            searchQuery={searchQuery}
-          />
-        </table>
+        <CList
+          data={list}
+          filters={filters}
+          requestNewData={(_, newList) => this.setState({ list: newList })}
+          navigate={navigate}
+          translations={translations}
+          searchQuery={searchQuery}
+        />
 
         {searchQuery && !list.count() ? <strong>{this.t('crd:contracts:top-search:nothingFound')}</strong> : null}
       </div>
