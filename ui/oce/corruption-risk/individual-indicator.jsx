99c6170b900fe06c6cb23143c76415762bb66ea1
Alexei Savca <savca.alexei@gmail.com>
2017-12-05 20:21:06 +0200

    OCE-395 Linted individual indicator page

--- ui/oce/corruption-risk/individual-indicator.jsx
+++ ui/oce/corruption-risk/individual-indicator.jsx
@@ -1,35 +1,33 @@
-import CustomPopupChart from "./custom-popup-chart";
-import {Map} from "immutable";
-import {pluckImm} from "../tools";
-import Table from "../visualizations/tables/index";
-import translatable from "../translatable";
-import CRDPage from "./page";
-import {colorLuminance} from "./tools";
+import CustomPopupChart from './custom-popup-chart';
+import { pluckImm } from '../tools';
+import translatable from '../translatable';
+import CRDPage from './page';
+import { colorLuminance } from './tools';
 
-class IndividualIndicatorChart extends CustomPopupChart{
-  getCustomEP(){
-    const {indicator} = this.props;
+class IndividualIndicatorChart extends CustomPopupChart {
+  getCustomEP() {
+    const { indicator } = this.props;
     return `flags/${indicator}/stats`;
   }
 
-  getData(){
+  getData() {
     const data = super.getData();
-    const {traceColors} = this.props.styling.charts;
-    if(!data) return [];
-    const {monthly} = this.props;
-    let dates = monthly ?
-                data.map(datum => {
-                  const month = datum.get('month');
-                  return this.t(`general:months:${month}`);
-                }).toJS() :
-                data.map(pluckImm('year')).toJS();
+    const { traceColors } = this.props.styling.charts;
+    if (!data) return [];
+    const { monthly } = this.props;
+    const dates = monthly ?
+      data.map((datum) => {
+        const month = datum.get('month');
+        return this.t(`general:months:${month}`);
+      }).toJS() :
+      data.map(pluckImm('year')).toJS();
 
-    let totalTrueValues = data.map(pluckImm('totalTrue', 0)).toJS();
-    let totalPrecondMetValues = data.map(pluckImm('totalPrecondMet', 0)).toJS();
+    const totalTrueValues = data.map(pluckImm('totalTrue', 0)).toJS();
+    const totalPrecondMetValues = data.map(pluckImm('totalPrecondMet', 0)).toJS();
 
-    if(dates.length == 1){
-      dates.unshift("");
-      dates.push(" ");
+    if (dates.length === 1) {
+      dates.unshift('');
+      dates.push(' ');
       totalTrueValues.unshift(0);
       totalTrueValues.push(0);
       totalPrecondMetValues.unshift(0);
@@ -45,7 +43,7 @@ class IndividualIndicatorChart extends CustomPopupChart{
       hoverinfo: 'none',
       fillcolor: traceColors[0],
       line: {
-        color: colorLuminance(traceColors[0], -.3),
+        color: colorLuminance(traceColors[0], -0.3),
       },
     }, {
       x: dates,
@@ -56,52 +54,52 @@ class IndividualIndicatorChart extends CustomPopupChart{
       hoverinfo: 'none',
       fillcolor: traceColors[1],
       line: {
-        color: colorLuminance(traceColors[1], -.3)
-      }
+        color: colorLuminance(traceColors[1], -0.3),
+      },
     }];
   }
 
-  getLayout(){
+  getLayout() {
     return {
       legend: {
         orientation: 'h',
         xanchor: 'right',
         yanchor: 'bottom',
         x: 1,
-        y: 1
+        y: 1,
       },
       hovermode: 'closest',
       xaxis: {
         type: 'category',
-        showgrid: false
+        showgrid: false,
       },
-      yaxis: {}
-    }
+      yaxis: {},
+    };
   }
 
-  getPopup(){
-    const {indicator, monthly} = this.props;
-    const {popup} = this.state;
-    const {year} = popup;
+  getPopup() {
+    const { monthly } = this.props;
+    const { popup } = this.state;
+    const { year } = popup;
     const data = super.getData();
-    if(!data) return null;
+    if (!data) return null;
     let datum;
-    if(monthly){
-      datum = data.find(datum => {
+    if (monthly) {
+      datum = data.find((datum) => {
         const month = datum.get('month');
-        return year == this.t(`general:months:${month}`);
-      })
+        return year === this.t(`general:months:${month}`);
+      });
     } else {
-      datum = data.find(datum => datum.get('year') == year);
+      datum = data.find(datum => datum.get('year') === year);
     }
     return (
-      <div className="crd-popup" style={{top: popup.top, left: popup.left}}>
+      <div className="crd-popup" style={{ top: popup.top, left: popup.left }}>
         <div className="row">
           <div className="col-sm-12 info text-center">
             {year}
           </div>
           <div className="col-sm-12">
-            <hr/>
+            <hr />
           </div>
           <div className="col-sm-8 text-right title">{this.t('crd:indicatorPage:individualIndicatorChart:popup:procurementsFlagged')}</div>
           <div className="col-sm-4 text-left info">{datum.get('totalTrue')}</div>
@@ -112,16 +110,16 @@ class IndividualIndicatorChart extends CustomPopupChart{
           <div className="col-sm-8 text-right title">{this.t('crd:indicatorPage:individualIndicatorChart:popup:percentEligible')}</div>
           <div className="col-sm-4 text-left info">{datum.get('percentPrecondMet').toFixed(2)} %</div>
         </div>
-        <div className="arrow"/>
+        <div className="arrow" />
       </div>
-    )
+    );
   }
 }
 
 import ProcurementsTable from "./procurements-table";
 
-class ProjectTable extends ProcurementsTable{
-  getCustomEP(){
+class ProjectTable extends ProcurementsTable {
+  getCustomEP() {
     const {corruptionType, indicator} = this.props;
     return `flags/${indicator}/releases?pageSize=10&flagType=${corruptionType}`;
   }
@@ -131,37 +129,36 @@ class ProjectTable extends ProcurementsTable{
   }
 }
 
-class IndividualIndicatorPage extends translatable(CRDPage){
-  constructor(...args){
+class IndividualIndicatorPage extends translatable(CRDPage) {
+  constructor(...args) {
     super(...args);
-    this.state = {
-    }
+    this.state = {};
   }
 
-  render(){
-    const {chart, table} = this.state;
+  render() {
+    const { chart, table } = this.state;
     const { corruptionType, indicator, translations, filters, years, monthly, months, width
-    , styling, navigate } = this.props;
+      , styling, navigate } = this.props;
     return (
       <div className="page-corruption-type">
         <h2 className="page-header">{this.t(`crd:indicators:${indicator}:name`)}</h2>
         <p className="definition">
-          <strong>{this.t("crd:indicators:general:indicator")}</strong>
+          <strong>{this.t('crd:indicators:general:indicator')}</strong>
           &nbsp;
           {this.t(`crd:indicators:${indicator}:indicator`)}
         </p>
         <p className="definition">
-          <strong>{this.t("crd:indicators:general:eligibility")}</strong>
+          <strong>{this.t('crd:indicators:general:eligibility')}</strong>
           &nbsp;
           {this.t(`crd:indicators:${indicator}:eligibility`)}
         </p>
         <p className="definition">
-          <strong>{this.t("crd:indicators:general:thresholds")}</strong>
+          <strong>{this.t('crd:indicators:general:thresholds')}</strong>
           &nbsp;
           {this.t(`crd:indicators:${indicator}:thresholds`)}
         </p>
         <p className="definition">
-          <strong>{this.t("crd:indicators:general:description")}</strong>
+          <strong>{this.t('crd:indicators:general:description')}</strong>
           &nbsp;
           {this.t(`crd:indicators:${indicator}:description`)}
         </p>
@@ -176,11 +173,11 @@ class IndividualIndicatorPage extends translatable(CRDPage){
             years={years}
             monthly={monthly}
             months={months}
-            requestNewData={(_, data) => this.setState({chart: data})}
+            requestNewData={(_, data) => this.setState({ chart: data })}
             data={chart}
             width={width - 20}
             styling={styling}
-            margin={{t: 0, b: 80, r: 40, pad: 40}}
+            margin={{ t: 0, b: 80, r: 40, pad: 40 }}
           />
         </section>
         <section>
@@ -190,7 +187,7 @@ class IndividualIndicatorPage extends translatable(CRDPage){
           <ProjectTable
             indicator={indicator}
             corruptionType={corruptionType}
-            requestNewData={(_, data) => this.setState({table: data})}
+            requestNewData={(_, data) => this.setState({ table: data })}
             data={table}
             translations={translations}
             filters={filters}
@@ -201,7 +198,7 @@ class IndividualIndicatorPage extends translatable(CRDPage){
           />
         </section>
       </div>
-    )
+    );
   }
 }
 
