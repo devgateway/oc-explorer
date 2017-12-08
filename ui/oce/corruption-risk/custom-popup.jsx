e9daf64cbca6cf5a21ac4f11734d17b229a4da47
Alexei Savca <savca.alexei@gmail.com>
2017-12-08 05:17:05 +0200

    OCE-405 Finished new custom popup component

--- ui/oce/corruption-risk/custom-popup.jsx
+++ ui/oce/corruption-risk/custom-popup.jsx
@@ -1,10 +1,46 @@
+import ReactDOM from 'react-dom';
+
 class CustomPopup extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = this.state || {};
+    this.state = {
+      show: true,
+      x: 0,
+      y: 0
+    }
+  }
+
+  componentDidMount() {
+    super.componentDidMount && super.componentDidMount();
+    const chartContainer = ReactDOM.findDOMNode(this)
+      .querySelector('.js-plotly-plot');
+
+    chartContainer.on('plotly_hover', e => this.setState({ show: true }));
+    chartContainer.on('plotly_unhover', e => this.setState({ show: false }));
+    chartContainer.addEventListener('mousemove', this.movePopup.bind(this));
+  }
+
+  movePopup({ offsetX: x, offsetY: y }) {
+    this.setState({ x, y });
+  }
+
   render() {
-    const { Chart } = this.props;
+    const { Popup, Chart } = this.props;
+    const { show, x, y } = this.state;
     return (
-      <Chart
-        {...this.props}
-      />
+      <div style={{ position: 'relative' }}>
+        {show &&
+          <Popup
+            {...this.props}
+            y={y}
+            x={x}
+          />
+        }
+        <Chart
+          {...this.props}
+        />
+      </div>
     );
   }
 }
