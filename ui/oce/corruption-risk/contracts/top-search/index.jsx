a4f089f89beec6b503b5f8aea4bc02d547242e69
Alexei Savca <savca.alexei@gmail.com>
2017-12-13 10:26:39 +0200

    OCE-407 Implemented exact search

--- ui/oce/corruption-risk/contracts/top-search/index.jsx
+++ ui/oce/corruption-risk/contracts/top-search/index.jsx
@@ -1,19 +1,38 @@
 import translatable from '../../../translatable';
+// eslint-disable-next-line no-unused-vars
 import style from './style.less';
+
+const isExactMatch = a => a.indexOf('"') === 0 &&
+      a.lastIndexOf('"') === a.length - 1 &&
+      a.length > 1;
 
 class TopSearch extends translatable(React.Component) {
   constructor(props, ...rest) {
     super(props, ...rest);
     this.state = {
       inputValue: props.searchQuery || '',
-      exactMatch: false
     };
+  }
+
+  toggleExactMatch() {
+    const { inputValue } = this.state;
+    const exactMatch = isExactMatch(inputValue);
+    const newValue = exactMatch ?
+      inputValue.slice(1, -1) :
+      `"${inputValue}"`;
+
+    this.setState(
+      { inputValue: newValue },
+      this.props.doSearch.bind(null, newValue),
+    );
   }
 
   render() {
     const { doSearch } = this.props;
     const { inputValue } = this.state;
     const hasSpecialChars = inputValue.indexOf('-') > -1;
+    const exactMatch = isExactMatch(inputValue);
+
     return (
       <div className="top-search">
         <div className="row">
@@ -36,21 +55,24 @@ class TopSearch extends translatable(React.Component) {
           <div className="col-sm-12">
             <div className="help-block">
               &bdquo;-&ldquo; is a special character meaning &bdquo;without&ldquo;.
-              <br/>
+              <br />
               For example
               &bdquo;
               <span className="foo">foo</span>
               &nbsp;-
               <span className="bar">bar</span>
               &ldquo;
-              will look for contracts containing <span className="foo">foo</span> but not <span className="bar">bar</span>.
-              <br/>
-              Enable literal search if this is not the desired behavior.
+              will look for contracts containing
+              &nbsp;
+              <span className="foo">foo</span> but not <span className="bar">bar</span>.
+              <br />
+              Use quotes or enable literal search if this is not the desired behavior.
             </div>
             <input
               id="exactMatch"
               type="checkbox"
-              value=""
+              checked={exactMatch}
+              onChange={() => this.toggleExactMatch()}
             />
             &nbsp;
             <label htmlFor="exactMatch">
