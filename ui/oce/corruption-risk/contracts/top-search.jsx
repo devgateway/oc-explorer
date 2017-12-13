08c2465ea40519fdb7e277eca5e25da44d8dfaa2
Alexei Savca <savca.alexei@gmail.com>
2017-12-13 08:20:25 +0200

    OCE-407 Controlling top search input

--- ui/oce/corruption-risk/contracts/top-search.jsx
+++ ui/oce/corruption-risk/contracts/top-search.jsx
@@ -1,22 +1,26 @@
 import translatable from '../../translatable';
 
 class TopSearch extends translatable(React.Component) {
-  componentDidMount(){
-    const { searchQuery } = this.props;
-    if (searchQuery) this.input.value = searchQuery;
+  constructor(props, ...rest){
+    super(props, ...rest);
+    this.state = {
+      inputValue: props.searchQuery
+    }
   }
 
   render() {
     const { doSearch } = this.props;
+    const { inputValue } = this.state;
     return (
       <div className="row">
-        <form action="javascript:void(0);" onSubmit={() => doSearch(this.input.value)}>
+        <form action="javascript:void(0);" onSubmit={() => doSearch(inputValue)}>
           <div className="input-group col-sm-4 top-search">
             <input
               type="text"
               className="form-control"
               placeholder={this.t('crd:contracts:top-search')}
-              ref={c => this.input = c}
+              value={inputValue}
+              onChange={e => this.setState({ inputValue: e.target.value })}
             />
             <div className="input-group-addon">
               <i className="glyphicon glyphicon-search"/>
