2959078e6b209403e8c45b0f7ebc9e3e222699e6
Alexei Savca <savca.alexei@gmail.com>
2017-12-25 17:04:45 +0200

    OCE-420 Linting

--- ui/oce/corruption-risk/crosstab.jsx
+++ ui/oce/corruption-risk/crosstab.jsx
@@ -1,37 +1,38 @@
-import Table from "../visualizations/tables/index";
+import Table from '../visualizations/tables/index';
 
-class Crosstab extends Table{
-  getCustomEP(){
-    const {indicators} = this.props;
+class Crosstab extends Table {
+  getCustomEP() {
+    const { indicators } = this.props;
     return indicators.map(indicator => `flags/${indicator}/crosstab`);
   }
 
-  componentDidUpdate(prevProps, ...args){
-    if(this.props.indicators != prevProps.indicators){
+  componentDidUpdate(prevProps, ...args) {
+    if (this.props.indicators !== prevProps.indicators) {
       this.fetch();
     }
     super.componentDidUpdate(prevProps, ...args);
   }
 
-  transform(data){
-    const {indicators} = this.props;
-    let matrix = {}, x = 0, y = 0;
-    for(x = 0; x<indicators.length; x++){
+  transform(data) {
+    const { indicators } = this.props;
+    const matrix = {};
+    let y = 0;
+    for (let x = 0; x < indicators.length; x++) {
       const xIndicatorID = indicators[x];
       matrix[xIndicatorID] = {};
       const datum = data[x][0];
-      for(y = 0; y<indicators.length; y++){
+      for (y = 0; y < indicators.length; y++) {
         const yIndicatorID = indicators[y];
-        if(datum){
+        if (datum) {
           matrix[xIndicatorID][yIndicatorID] = {
             count: datum[yIndicatorID],
-            percent: datum.percent[yIndicatorID]
-          }
+            percent: datum.percent[yIndicatorID],
+          };
         } else {
           matrix[xIndicatorID][yIndicatorID] = {
             count: 0,
-            percent: 0
-          }
+            percent: 0,
+          };
         }
       }
     }
@@ -46,50 +47,50 @@ class Crosstab extends Table{
         <td>{rowIndicatorName}</td>
         <td className="nr-flags">{rowData.getIn([rowIndicatorID, 'count'])}</td>
         {rowData.map((datum, indicatorID) => {
-           const indicatorName = this.t(`crd:indicators:${indicatorID}:name`);
-           const indicatorDescription = this.t(`crd:indicators:${indicatorID}:indicator`);
-           if(indicatorID == rowIndicatorID){
-             return <td className="not-applicable" key={indicatorID}>&mdash;</td>
-           } else {
-             const percent = datum.get('percent');
-             const count = datum.get('count');
-             const color = Math.round(255 - 255 * (percent/100))
-             const style = {backgroundColor: `rgb(${color}, 255, ${color})`}
-             return (
-               <td key={indicatorID} className="hoverable" style={style}>
-                 {percent && percent.toFixed(2)} %
-                 <div className="crd-popup text-left">
-                   <div className="row">
-                     <div className="col-sm-12 info">
-                       {this.t('crd:corruptionType:crosstab:popup:percents')
-                         .replace('$#$', percent.toFixed(2))
-                         .replace('$#$', rowIndicatorName)
-                         .replace('$#$', indicatorName)}
-                     </div>
-                     <div className="col-sm-12">
-                       <hr/>
-                     </div>
-                     <div className="col-sm-12 info">
-                       <h4>{this.t('crd:corruptionType:crosstab:popup:count').replace('$#$', count)}</h4>
-                       <p><strong>{rowIndicatorName}</strong>: {rowIndicatorDescription}</p>
-                       <p className="and">{this.t('crd:corruptionType:crosstab:popup:and')}</p>
-                       <p><strong>{indicatorName}</strong>: {indicatorDescription}</p>
-                     </div>
-                   </div>
-                   <div className="arrow"/>
-                 </div>
-               </td>
-             )
-           }
+          const indicatorName = this.t(`crd:indicators:${indicatorID}:name`);
+          const indicatorDescription = this.t(`crd:indicators:${indicatorID}:indicator`);
+          if (indicatorID === rowIndicatorID) {
+            return <td className="not-applicable" key={indicatorID}>&mdash;</td>;
+          } else {
+            const percent = datum.get('percent');
+            const count = datum.get('count');
+            const color = Math.round(255 - 255 * (percent / 100));
+            const style = { backgroundColor: `rgb(${color}, 255, ${color})` };
+            return (
+              <td key={indicatorID} className="hoverable" style={style}>
+                {percent && percent.toFixed(2)} %
+                <div className="crd-popup text-left">
+                  <div className="row">
+                    <div className="col-sm-12 info">
+                      {this.t('crd:corruptionType:crosstab:popup:percents')
+                        .replace('$#$', percent.toFixed(2))
+                        .replace('$#$', rowIndicatorName)
+                        .replace('$#$', indicatorName)}
+                    </div>
+                    <div className="col-sm-12">
+                      <hr />
+                    </div>
+                    <div className="col-sm-12 info">
+                      <h4>{this.t('crd:corruptionType:crosstab:popup:count').replace('$#$', count)}</h4>
+                      <p><strong>{rowIndicatorName}</strong>: {rowIndicatorDescription}</p>
+                      <p className="and">{this.t('crd:corruptionType:crosstab:popup:and')}</p>
+                      <p><strong>{indicatorName}</strong>: {indicatorDescription}</p>
+                    </div>
+                  </div>
+                  <div className="arrow" />
+                </div>
+              </td>
+            );
+          }
         }).toArray()}
       </tr>
     )
   }
 
-  render(){
-    const {indicators, data} = this.props;
-    if(!data) return null;
-    if(!data.count()) return null;
+  render() {
+    const { data } = this.props;
+    if (!data) return null;
+    if (!data.count()) return null;
     return (
       <table className="table table-striped table-hover table-bordered table-crosstab">
         <thead>
@@ -103,7 +104,7 @@ class Crosstab extends Table{
           {data.map((rowData, indicatorID) => this.row(rowData, indicatorID)).toArray()}
         </tbody>
       </table>
-    )
+    );
   }
 }
 
