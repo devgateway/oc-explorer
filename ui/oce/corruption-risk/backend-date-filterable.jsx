e5ffe95bb48bb94b12f54e636525fd0ee048f4f8
Alexei Savca <savca.alexei@gmail.com>
2018-01-05 14:15:57 +0200

    OCE-439 Added BackendDateFilterable HOC decorator

---
+++ ui/oce/corruption-risk/backend-date-filterable.jsx
@@ -0,0 +1,40 @@
+import { Map, Set } from 'immutable';
+import { cloneChild } from './tools';
+
+class BackendDateFilterable extends React.PureComponent {
+  constructor(...args){
+    super(...args);
+    this.state = this.state || {};
+    this.state.decoratedFilters = this.decorateFilters(this.props);
+  }
+
+  decorateFilters({ filters, years, months }) {
+    const monthly = years.count() === 1;
+    return filters
+      .set('year', years)
+      .set('monthly', monthly)
+      .set('month', monthly && months.count() != 12 ? months : Set())
+    ;
+  }
+
+  componentWillUpdate(nextProps) {
+    if (['filters', 'years', 'months'].some(prop => this.props[prop] != nextProps[prop])) {
+      this.setState({
+        decoratedFilters: this.decorateFilters(nextProps),
+      })
+    }
+  }
+
+  render() {
+    const decoratedProps = Object.assign({}, this.props, {
+      filters: this.state.decoratedFilters,
+    });
+    delete decoratedProps.years;
+    delete decoratedProps.months;
+    delete decoratedProps.monthly;
+    const { years } = this.props;
+    return cloneChild(this, decoratedProps);
+  }
+}
+
+export default BackendDateFilterable;
