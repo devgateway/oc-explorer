c6112aaa0f688e853cb9cc4f4723cae159c35ace
Alexei Savca <savca.alexei@gmail.com>
2018-01-29 15:12:41 +0200

    OCE-439 Wins and losses popup

--- ui/oce/corruption-risk/suppliers/single/bars/wins-and-losses.jsx
+++ ui/oce/corruption-risk/suppliers/single/bars/wins-and-losses.jsx
@@ -2,8 +2,70 @@ import PlotlyChart from '../../../plotly-chart';
 import DataFetcher from '../../../data-fetcher';
 import { wireProps } from '../../../tools';
 import { pluck } from '../../../../tools';
+import CustomPopup from '../../../custom-popup';
+
+const POPUP_WIDTH = 300;
+const POPUP_HEIGHT = 75;
+const POPUP_ARROW_SIZE = 8;
+
+class Popup extends React.PureComponent {
+  render() {
+    const { x, y, points } = this.props;
+    const [a, b] = points;
+    const point = a.y > b.y ? a : b;
+    const { xaxis, yaxis } = point;
+    const markerLeft = xaxis.l2p(point.pointNumber) + xaxis._offset;
+    const markerTop = yaxis.l2p(point.y) + yaxis._offset;
+
+    const left = markerLeft - (POPUP_WIDTH / 2);
+    const top = markerTop - POPUP_HEIGHT - (POPUP_ARROW_SIZE * 1.5);
+
+    const style = {
+      left,
+      top,
+      width: POPUP_WIDTH,
+      height: POPUP_HEIGHT
+    };
+
+    return (
+      <div
+        className="crd-popup donut-popup text-center"
+        style={style}
+      >
+        test
+        <div className="arrow"/>
+      </div>
+    )
+  }
+}
 
 class WinsBarChart extends React.PureComponent {
+  render() {
+    const { width, data } = this.props;
+    return (
+      <PlotlyChart
+        data={data}
+        layout={{
+          width,
+          height: 250,
+          barmode: 'group',
+          margin: {t: 0, r: 20, b: 30, l: 20, pad: 0},
+          paper_bgcolor: 'rgba(0, 0, 0, 0)',
+          plot_bgcolor: 'rgba(0, 0, 0, 0)',
+          legend: {
+            xanchor: 'right',
+            yanchor: 'top',
+            x: .9,
+            y: 1.5,
+            orientation: 'h',
+          }
+        }}
+      />
+    );
+  }
+}
+
+class WinsBarChartWrapper extends React.PureComponent {
   onRequestNewData(path, data) {
     const names = data.map(pluck('procuringEntityName'));
     this.props.requestNewData(path, [{
@@ -14,6 +76,7 @@ class WinsBarChart extends React.PureComponent {
       marker: {
         color: '#289df4',
       },
+      hoverinfo: 'none'
     }, {
       x: names,
       y: data.map(pluck('countFlags')),
@@ -22,38 +85,28 @@ class WinsBarChart extends React.PureComponent {
       marker: {
         color: '#ce4747',
       },
+      hoverinfo: 'none'
     }]);
   }
 
   render() {
-    const { width, requestNewData } = this.props;
+    const { requestNewData } = this.props;
     if (!requestNewData) return null;
+
     return (
       <DataFetcher
-        {...wireProps(this)}
+        {...this.props}
         endpoint="supplierWinsPerProcuringEntity"
         requestNewData={this.onRequestNewData.bind(this)}
       >
-        <PlotlyChart
-          layout={{
-            width,
-            height: 250,
-            barmode: 'group',
-            margin: {t: 0, r: 20, b: 30, l: 20, pad: 0},
-            paper_bgcolor: 'rgba(0, 0, 0, 0)',
-            plot_bgcolor: 'rgba(0, 0, 0, 0)',
-            legend: {
-              xanchor: 'right',
-              yanchor: 'top',
-              x: .9,
-              y: 1.5,
-              orientation: 'h',
-            }
-          }}
+        <CustomPopup
+          {...this.props}
+          Chart={WinsBarChart}
+          Popup={Popup}
         />
       </DataFetcher>
     );
   }
 }
 
-export default WinsBarChart;
+export default WinsBarChartWrapper;
