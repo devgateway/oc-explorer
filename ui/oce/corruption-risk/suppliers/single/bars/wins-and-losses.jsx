2f4067c39bb171aa2364da6b68bd80453e8553ea
Alexei Savca <savca.alexei@gmail.com>
2018-01-31 19:31:57 +0200

    OCE-444 Wins and losses restyle

--- ui/oce/corruption-risk/suppliers/single/bars/wins-and-losses.jsx
+++ ui/oce/corruption-risk/suppliers/single/bars/wins-and-losses.jsx
@@ -1,24 +1,30 @@
+import ReactDOM from 'react-dom';
 import PlotlyChart from '../../../plotly-chart';
 import DataFetcher from '../../../data-fetcher';
 import { wireProps } from '../../../tools';
 import { pluck } from '../../../../tools';
 import CustomPopup from '../../../custom-popup';
 
-const POPUP_WIDTH = 300;
-const POPUP_HEIGHT = 55;
 const POPUP_ARROW_SIZE = 8;
 
 class Popup extends React.PureComponent {
   render() {
     const { x, y, points } = this.props;
     const [a, b] = points;
-    const point = a.y > b.y ? a : b;
+    const point = a.x < b.x ? a : b;
     const { xaxis, yaxis } = point;
-    const markerLeft = xaxis.l2p(point.pointNumber) + xaxis._offset;
-    const markerTop = yaxis.l2p(point.y) + yaxis._offset;
+    const markerLeft = xaxis.l2p(point.x) + xaxis._offset;
+    const markerTop = yaxis.l2p(point.pointNumber) + yaxis._offset;
 
-    const left = markerLeft - (POPUP_WIDTH / 2);
-    const top = markerTop - POPUP_HEIGHT - (POPUP_ARROW_SIZE * 1.5);
+    const PEname = points[0].y;
+
+    let POPUP_HEIGHT = 70;
+    let POPUP_WIDTH = 300;
+
+    if (PEname.length > 40 ) POPUP_HEIGHT = 90;
+
+    const left = (markerLeft / 2) - (POPUP_WIDTH / 2);
+    const top = markerTop - POPUP_HEIGHT - (POPUP_ARROW_SIZE * 2);
 
     const style = {
       left,
@@ -27,14 +33,16 @@ class Popup extends React.PureComponent {
       height: POPUP_HEIGHT
     };
 
-    const wins = points[0].y;
-    const flags = points[1].y;
+    const wins = points[0].x;
+    const flags = points[1].x;
 
     return (
       <div
         className="crd-popup donut-popup text-center"
         style={style}
       >
+        {PEname}
+        <br />
         {wins} {wins === 1 ? 'win' : 'wins'}, {flags} {flags === 1 ? 'flag' : 'flags'}
         <div className="arrow"/>
       </div>
@@ -43,6 +51,22 @@ class Popup extends React.PureComponent {
 }
 
 class WinsBarChart extends React.PureComponent {
+  fixYLabels(...args) {
+    ReactDOM
+      .findDOMNode(this)
+      .querySelectorAll('.ytick')
+      .forEach(label => {
+        const { width, height } = label.getBoundingClientRect();
+        label.setAttribute('transform', `translate(${width}, -${height})`)
+        if (navigator.userAgent.indexOf('Firefox') === -1) {
+          setTimeout(function() {
+            const { width, height } = label.getBoundingClientRect();
+            label.setAttribute('transform', `translate(${width + 5}, -${height + 5})`)
+          })
+        }
+      });
+  }
+
   render() {
     const { width, data } = this.props;
     return (
@@ -50,9 +74,8 @@ class WinsBarChart extends React.PureComponent {
         data={data}
         layout={{
           width,
-          height: 250,
-          barmode: 'group',
-          margin: {t: 0, r: 20, b: 30, l: 20, pad: 0},
+          height: 350,
+          margin: {t: 0, r: 0, b: 30, l: 20, pad: 0},
           paper_bgcolor: 'rgba(0, 0, 0, 0)',
           plot_bgcolor: 'rgba(0, 0, 0, 0)',
           legend: {
@@ -61,8 +84,11 @@ class WinsBarChart extends React.PureComponent {
             x: .9,
             y: 1.5,
             orientation: 'h',
-          }
+          },
+          barmode: 'group',
+          bargap: .5
         }}
+        onUpdate={this.fixYLabels.bind(this)}
       />
     );
   }
@@ -72,23 +98,25 @@ class WinsBarChartWrapper extends React.PureComponent {
   onRequestNewData(path, data) {
     const names = data.map(pluck('procuringEntityName'));
     this.props.requestNewData(path, [{
-      x: names,
-      y: data.map(pluck('count')),
+      x: data.map(pluck('count')),
+      y: names,
       name: 'Wins',
       type: 'bar',
       marker: {
         color: '#289df4',
       },
-      hoverinfo: 'none'
+      hoverinfo: 'none',
+      orientation: 'h',
     }, {
-      x: names,
-      y: data.map(pluck('countFlags')),
+      x: data.map(pluck('countFlags')),
+      y: names,
       name: 'Flags',
       type: 'bar',
       marker: {
         color: '#ce4747',
       },
-      hoverinfo: 'none'
+      hoverinfo: 'none',
+      orientation: 'h',
     }]);
   }
 
