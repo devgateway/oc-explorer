67706f3b5fd21685b8058efe4f69cbc6227491e9
Alexei Savca <savca.alexei@gmail.com>
2018-02-22 05:41:56 +0200

    OCE-478 Added supplierId and supplierFilters to state

--- ui/oce/state/oce-state.jsx
+++ ui/oce/state/oce-state.jsx
@@ -1,4 +1,4 @@
-import { Map } from 'immutable';
+import { Map, Set } from 'immutable';
 import State from './index';
 
 const state = new State();
@@ -12,6 +12,17 @@ state.map({
   name: 'datelessFilters',
   deps: ['filters'],
   mapper: filters => filters.delete('years').delete('months')
+});
+
+state.input({
+  name: 'supplierId'
+});
+
+state.map({
+  name: 'supplierFilters',
+  deps: ['filters', 'supplierId'],
+  mapper: (filters, supplierId) => 
+    filters.update('supplierId', Set(), supplierIds => supplierIds.add(supplierId))
 })
 
 export default state;
