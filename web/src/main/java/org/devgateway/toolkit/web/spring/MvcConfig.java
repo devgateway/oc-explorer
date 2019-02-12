d7bfe10e2ae0602ce8756c0010c24daf2d97fec8
Ionut Dobre <idobre@developmentgateway.org>
2019-02-12 13:59:18 +0200

    remove thymeleaf.

--- web/src/main/java/org/devgateway/toolkit/web/spring/MvcConfig.java
+++ web/src/main/java/org/devgateway/toolkit/web/spring/MvcConfig.java
@@ -19,19 +19,10 @@ import org.devgateway.toolkit.web.generators.GenericExcelKeyGenerator;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addViewControllers(final ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
-    }
 
     @Bean(name = "genericExcelKeyGenerator")
     public KeyGenerator genericExcelKeyGenerator(final ObjectMapper objectMapper) {
